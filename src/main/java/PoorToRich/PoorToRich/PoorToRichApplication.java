734b6ed5761918497c17b161bf03da76513db41e
belight2 <chlwnsgur090@gmail.com>
2025-02-17 15:53:28 +0900

    init project

---
+++ src/main/java/PoorToRich/PoorToRich/PoorToRichApplication.java
@@ -0,0 +1,13 @@
+package PoorToRich.PoorToRich;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoorToRichApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoorToRichApplication.class, args);
+	}
+
+}
