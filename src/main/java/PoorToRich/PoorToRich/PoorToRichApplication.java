0ff57a5a35a8b62412f07d18c27abc7f459c2d74
belight2 <chlwnsgur090@gmail.com>
2025-02-27 19:02:30 +0900

    Create Redis

--- src/main/java/PoorToRich/PoorToRich/PoorToRichApplication.java
+++ src/main/java/PoorToRich/PoorToRich/PoorToRichApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PoorToRichApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PoorToRichApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(PoorToRichApplication.class, args);
+    }
 }
