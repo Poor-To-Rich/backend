056c08826252fd3db5a007a748d9db5fbe1dd31f
sseen2 <kse10023@naver.com>
2025-03-03 23:25:17 +0900

    feat: 카테고리 service 추가
    - 지출 기본 카테고리 api에 필요한 서비스 로직 구현

---
+++ src/main/java/PoorToRich/PoorToRich/category/service/CategoryService.java
@@ -0,0 +1,30 @@
+package PoorToRich.PoorToRich.category.service;
+
+import PoorToRich.PoorToRich.category.entity.Category;
+import PoorToRich.PoorToRich.category.entity.CategoryType;
+import PoorToRich.PoorToRich.category.repository.CategoryRepository;
+import PoorToRich.PoorToRich.category.response.CategoryResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class CategoryService {
+
+    private final CategoryRepository categoryRepository;
+
+    public List<CategoryResponse> getExpenseCategories() {
+        return categoryRepository.findAll().stream()
+                .filter(category -> category.getType() == CategoryType.DEFAULT_EXPENSE)
+                .map(category -> CategoryResponse.builder()
+                        .name(category.getName())
+                        .color(category.getColor())
+                        .visibility(category.isVisibility())
+                        .build())
+                .collect(Collectors.toList());
+    }
+}
