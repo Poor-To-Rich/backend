f6e1b2953fe88a6c47a1d4232a8f9642f611bf66
sseen2 <kse10023@naver.com>
2025-03-03 23:23:47 +0900

    feat: 카테고리 response 추가
    - 기본 카테고리 response 추가
    - response에 필요한 카테고리 DTO 추가

---
+++ src/main/java/PoorToRich/PoorToRich/category/response/CategoryResponse.java
@@ -0,0 +1,14 @@
+package PoorToRich.PoorToRich.category.response;
+
+import lombok.*;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategoryResponse {
+
+    private String color;
+    private String name;
+    private boolean visibility;
+}
