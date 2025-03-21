b2a5b92ee71be1c69dd4f073cad4d80458db65cd
belight2 <chlwnsgur090@gmail.com>
2025-03-21 19:32:43 +0900

    feat: UserResponse 추가
    User관련 서비스 이용 시 반환할 Response를 enum 형식으로 추가

---
+++ src/main/java/com/poortorich/user/response/enums/UserResponse.java
@@ -0,0 +1,29 @@
+package com.poortorich.user.response.enums;
+
+import com.poortorich.global.response.Response;
+import com.poortorich.user.constants.UserConstant.ValidationMessages;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@RequiredArgsConstructor
+public enum UserResponse implements Response {
+
+    REGISTRATION_SUCCESS(HttpStatus.CREATED, "회원 가입 성공"),
+
+    NICKNAME_DUPLICATE(HttpStatus.CONFLICT, ValidationMessages.NICKNAME_DUPLICATE),
+    EMAIL_DUPLICATE(HttpStatus.CONFLICT, ValidationMessages.EMAIL_DUPLICATE),
+    PASSWORD_NOT_MATCH(HttpStatus.BAD_REQUEST, ValidationMessages.PASSWORD_DO_NOT_MATCH);
+
+    private final HttpStatus httpStatus;
+    private final String message;
+
+    @Override
+    public HttpStatus getHttpStatus() {
+        return null;
+    }
+
+    @Override
+    public String getMessage() {
+        return "";
+    }
+}
