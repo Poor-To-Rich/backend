af4c71fa9e96f4826e7da0c13c13f55090be3975
belight2 <chlwnsgur090@gmail.com>
2025-03-22 15:53:50 +0900

    feat: S3Constatns 추가
    파일 업로드 서비스에서 사용되는 문자열 상수화 클래스 생성

---
+++ src/main/java/com/poortorich/s3/constants/S3Constants.java
@@ -0,0 +1,31 @@
+package com.poortorich.s3.constants;
+
+
+import java.util.Arrays;
+import java.util.List;
+
+public class S3Constants {
+
+    private S3Constants() {
+    }
+
+    public static final class Config {
+        public static final String BUCKET = "${cloud.aws.s3.bucket}";
+        public static final String REGION = "${cloud.aws.region.static}";
+        public static final String ACCESS_KEY = "${cloud.aws.credentials.accessKey}";
+        public static final String SECRET_KEY = "${cloud.aws.credentials.secretKey}";
+    }
+
+    public static final class ValidationMessages {
+        public static final String INVALID_FILE_TYPE = "지원되지 않는 이미지 형식입니다. JPG, JPEG, PNG 파일만 허용됩니다.";
+        public static final String FILE_SIZE_INVALID = "파일 크기가 너무 큽니다. 5MB 이하의 파일만 허용됩니다.";
+    }
+
+    public static final class ValidationConstraints {
+        public static final List<String> ALLOWED_FILE_TYPES = Arrays.asList(
+                "image/jpeg", "image/jpg", "image/png"
+        );
+
+        public static final long FILE_MAX_SIZE = 5 * 1024 * 1024;
+    }
+}
