832fe38e6bfc3dbcd0ce57b7ffcd95639b9951a6
belight2 <chlwnsgur090@gmail.com>
2025-03-27 15:27:26 +0900

    feat: SecurityConfig 추가

--- src/main/java/com/poortorich/security/config/SecurityConfig.java
+++ src/main/java/com/poortorich/security/config/SecurityConfig.java
@@ -1,25 +1,57 @@
-package com.poortorich.security;
+package com.poortorich.security.config;
 
+import com.poortorich.security.constants.SecurityConstants;
+import com.poortorich.user.constants.UserControllerConstants;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) {
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(
+                                UserControllerConstants.REGISTER_PATH,
+                                UserControllerConstants.CHECK_USERNAME_DUPLICATE_PATH,
+                                "/email/send",
+                                "/email/verify",
+                                "/email/block"
+                        ).permitAll()
+
+                        .anyRequest().denyAll()
+                );
+
+        return http.build();
     }
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration configuration = new CorsConfiguration();
 
-        
+        configuration.setAllowedOrigins(SecurityConstants.ALLOWED_ORIGINS);
+        configuration.setAllowedMethods(SecurityConstants.ALLOWED_METHOD);
+        configuration.setAllowedHeaders(SecurityConstants.ALLOWED_HEADERS);
+        configuration.setAllowCredentials(Boolean.TRUE);
+
+        source.registerCorsConfiguration(SecurityConstants.CORS_ALL_PATH, configuration);
+
+        return source;
     }
 }
--- src/main/java/com/poortorich/security/constants/SecurityConstants.java
+++ src/main/java/com/poortorich/security/constants/SecurityConstants.java
@@ -1,4 +1,15 @@
 package com.poortorich.security.constants;
 
+import java.util.List;
+
 public class SecurityConstants {
+
+    public static final List<String> ALLOWED_ORIGINS = List.of("http://localhost:8080");
+    public static final List<String> ALLOWED_METHOD = List.of("GET", "POST", "PUT", "DELETE");
+    public static final List<String> ALLOWED_HEADERS = List.of("authorization", "content-type", "x-auth-token");
+
+    public static final String CORS_ALL_PATH = "/**";
+
+    private SecurityConstants() {
+    }
 }
