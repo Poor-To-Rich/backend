809795052ee7626728ccb2709117ef09d9783b74
belight2 <chlwnsgur090@gmail.com>
2025-04-05 19:36:58 +0900

    feat: Jwt Token을 제공하는 JwtTokenProvider 추가

--- src/main/java/com/poortorich/auth/service/JwtTokenProvider.java
+++ src/main/java/com/poortorich/auth/service/JwtTokenProvider.java
@@ -1,4 +1,47 @@
 package com.poortorich.auth.service;
 
+import com.poortorich.auth.constants.JwtConstants;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import java.util.Date;
+import javax.crypto.SecretKey;
+import lombok.RequiredArgsConstructor;
+import org.apache.commons.codec.binary.Base64;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class JwtTokenProvider {
+
+    private final UserDetailsService userDetailsService;
+
+    public String generateAccessToken(String username) {
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + JwtConstants.ACCESS_TOKEN_EXPIRATION_TIME);
+
+        return Jwts.builder()
+                .subject(username)
+                .issuedAt(now)
+                .expiration(expiryDate)
+                .signWith(getSecretKey())
+                .compact();
+    }
+
+    public String generateRefreshToken(String username) {
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + JwtConstants.REFRESH_TOKEN_EXPIRATION_TIME);
+
+        return Jwts.builder()
+                .subject(username)
+                .issuedAt(now)
+                .expiration(expiryDate)
+                .signWith(getSecretKey())
+                .compact();
+    }
+    
+    private SecretKey getSecretKey() {
+        byte[] keyBytes = Base64.decodeBase64(JwtConstants.SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
 }
---
+++ src/main/java/com/poortorich/auth/util/JwtUtils.java
@@ -0,0 +1,4 @@
+package com.poortorich.auth.util;
+
+public class JwtUtils {
+}
