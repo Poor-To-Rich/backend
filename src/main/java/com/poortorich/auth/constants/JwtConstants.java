6e8c23bfeade285f27947a6766706fea9bd00575
belight2 <chlwnsgur090@gmail.com>
2025-04-05 17:59:53 +0900

    refactor: jwt에서 auth로 패키지명 변경

--- src/main/java/com/poortorich/auth/constants/JwtConstants.java
+++ src/main/java/com/poortorich/auth/constants/JwtConstants.java
@@ -1,17 +1,14 @@
-package com.poortorich.jwt.constants;
+package com.poortorich.auth.constants;
 
 public class JwtConstants {
 
     // 1시간
     public static final long ACCESS_TOKEN_EXPIRATION_TIME = 60 * 60 * 1000L;
-
     // 7일
     public static final long REFRESH_TOKEN_EXPIRATION_TIME = 7 * 24 * 60 * 60 * 1000L;
 
     public static final String SECRET_KEY = "{jwt.secret.key}";
-
     public static final String TOKEN_PREFIX = "Bearer ";
-
     public static final String HEADER_AUTHORIZATION = "Authorization";
 
     private JwtConstants() {
