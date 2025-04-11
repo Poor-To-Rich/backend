f43764628f1759d7a44b75c5377cdd1ad1ddd70f
belight2 <chlwnsgur090@gmail.com>
2025-04-11 13:23:51 +0900

    feat: 로그인 요청 DTO 추가

--- src/main/java/com/poortorich/auth/request/LoginRequest.java
+++ src/main/java/com/poortorich/auth/request/LoginRequest.java
@@ -1,4 +1,19 @@
 package com.poortorich.auth.request;
 
+import com.poortorich.auth.constants.AuthResponseMessage;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class LoginRequest {
+
+    @NotBlank(message = AuthResponseMessage.USERNAME_REQUIRED)
+    private String username;
+
+    @NotBlank(message = AuthResponseMessage.PASSWORD_REQUIRED)
+    private String password;
 }
