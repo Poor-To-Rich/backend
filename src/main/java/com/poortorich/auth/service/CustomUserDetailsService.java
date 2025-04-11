2806e0167723037cf100bdb593bb254fd347979d
belight2 <chlwnsgur090@gmail.com>
2025-04-11 13:22:39 +0900

    feat: 인증 로직에서 사용할 UserDetailsService 추가

--- src/main/java/com/poortorich/auth/service/CustomUserDetailsService.java
+++ src/main/java/com/poortorich/auth/service/CustomUserDetailsService.java
@@ -1,4 +1,22 @@
 package com.poortorich.auth.service;
 
-public class UserDetailsServiceImpl {
+import com.poortorich.auth.response.enums.AuthResponse;
+import com.poortorich.global.exceptions.UnauthorizedException;
+import com.poortorich.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) {
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new UnauthorizedException(AuthResponse.TOKEN_INVALID));
+    }
 }
