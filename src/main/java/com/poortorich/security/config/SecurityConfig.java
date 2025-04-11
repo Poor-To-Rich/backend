20b29fbea12ac26b38f29ffd21fd3838fe8ee78c
belight2 <chlwnsgur090@gmail.com>
2025-04-11 13:30:51 +0900

    feat: JWT 토큰 인증 필터 추가

--- src/main/java/com/poortorich/security/config/SecurityConfig.java
+++ src/main/java/com/poortorich/security/config/SecurityConfig.java
@@ -1,22 +1,34 @@
 package com.poortorich.security.config;
 
 import com.poortorich.security.constants.SecurityConstants;
+import com.poortorich.security.filter.auth.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final UserDetailsService userDetailsService;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -27,15 +39,10 @@ public class SecurityConfig {
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(
-                                "user/register",
-                                "user/register/username/exists",
-                                "/email/send",
-                                "/email/verify",
-                                "/email/block"
-                        ).permitAll()
-                        .anyRequest().denyAll()
-                );
+                        .requestMatchers(SecurityConstants.PERMIT_ALL_ENDPOINTS).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
@@ -58,5 +65,19 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config)
+            throws Exception {
+        return config.getAuthenticationManager();
     }
 }
--- src/main/java/com/poortorich/security/filter/auth/JwtAuthenticationFilter.java
+++ src/main/java/com/poortorich/security/filter/auth/JwtAuthenticationFilter.java
@@ -1,4 +1,134 @@
 package com.poortorich.security.filter.auth;
 
-public class JwtAuthenticationFilter {
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.poortorich.auth.jwt.util.JwtCookieManager;
+import com.poortorich.auth.jwt.util.JwtTokenExtractor;
+import com.poortorich.auth.jwt.validator.JwtTokenValidator;
+import com.poortorich.auth.response.enums.AuthResponse;
+import com.poortorich.global.exceptions.UnauthorizedException;
+import com.poortorich.global.response.BaseResponse;
+import com.poortorich.global.response.Response;
+import com.poortorich.security.constants.SecurityConstants;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    private final JwtTokenValidator tokenValidator;
+    private final JwtTokenExtractor tokenExtractor;
+    private final JwtCookieManager cookieManager;
+    private final UserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+
+        if (Arrays.asList(SecurityConstants.PERMIT_ALL_ENDPOINTS).contains(request.getRequestURI())) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        try {
+            if (!processAccessToken(request, response)) {
+                processRefreshToken(request, response);
+            }
+        } catch (UnauthorizedException exception) {
+            cookieManager.clearAuthCookie(response);
+            setResponseMessage(response, exception.getResponse());
+            return;
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private void processRefreshToken(HttpServletRequest request, HttpServletResponse response) {
+        Optional<String> refreshToken = cookieManager.extractRefreshTokenFromCookies(request);
+
+        if (refreshToken.isEmpty()) {
+            throw new UnauthorizedException(AuthResponse.TOKEN_INVALID);
+        }
+
+        tokenValidator.validateToken(refreshToken.get());
+        UserDetails userDetails = loadUserDetailsFromToken(refreshToken.get());
+        setAuthentication(userDetails, request);
+    }
+
+    private boolean processAccessToken(HttpServletRequest request, HttpServletResponse response) {
+        Optional<String> accessToken = cookieManager.extractAccessTokenFromCookies(request);
+
+        if (accessToken.isEmpty()) {
+            throw new UnauthorizedException(AuthResponse.TOKEN_INVALID);
+        }
+
+        try {
+            if (tokenValidator.isTokenExpired(accessToken.get())) {
+                throw new UnauthorizedException(AuthResponse.ACCESS_TOKEN_EXPIRED);
+            }
+            UserDetails userDetails = loadUserDetailsFromToken(accessToken.get());
+            setAuthentication(userDetails, request);
+        } catch (UnauthorizedException exception) {
+            if (exception.getResponse().equals(AuthResponse.ACCESS_TOKEN_EXPIRED)) {
+                throw exception;
+            }
+            return false;
+        }
+
+        return true;
+    }
+
+
+    private UserDetails loadUserDetailsFromToken(String token) {
+        String username = tokenExtractor.extractUsername(token);
+
+        if (username == null) {
+            throw new UnauthorizedException(AuthResponse.TOKEN_INVALID);
+        }
+
+        return userDetailsService.loadUserByUsername(username);
+    }
+
+    private void setAuthentication(UserDetails userDetails, HttpServletRequest request) {
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                userDetails, null, userDetails.getAuthorities()
+        );
+
+        authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+    }
+
+    private void setResponseMessage(HttpServletResponse response, Response responseInformation) throws IOException {
+        response.setStatus(responseInformation.getHttpStatus().value());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setCharacterEncoding("UTF-8");
+
+        BaseResponse responseMessage = BaseResponse
+                .builder()
+                .resultCode(responseInformation.getHttpStatus().value())
+                .resultMessage(responseInformation.getMessage())
+                .build();
+
+        objectMapper.writeValue(response.getWriter(), responseMessage);
+    }
 }
