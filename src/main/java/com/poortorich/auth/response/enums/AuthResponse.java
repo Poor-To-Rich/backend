42563fd5dd441dc39c5d530444ceadb383f17d50
belight2 <chlwnsgur090@gmail.com>
2025-04-11 13:19:18 +0900

    feat: 인증 Response 추가

--- src/main/java/com/poortorich/auth/response/enums/AuthResponse.java
+++ src/main/java/com/poortorich/auth/response/enums/AuthResponse.java
@@ -1,13 +1,21 @@
-package com.poortorich.auth.jwt.response.enums;
+package com.poortorich.auth.response.enums;
 
+import com.poortorich.auth.constants.AuthResponseMessage;
 import com.poortorich.global.response.Response;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 
 @AllArgsConstructor
 public enum AuthResponse implements Response {
-    TOKEN_INVALID(HttpStatus.UNAUTHORIZED, "인증 정보가 유효하지 않습니다. 다시 로그인해 주세요."),
-    LOGIN_SUCCESS(HttpStatus.OK, "로그인 성공");
+    TOKEN_INVALID(HttpStatus.UNAUTHORIZED, AuthResponseMessage.TOKEN_INVALID),
+    CREDENTIALS_INVALID(HttpStatus.BAD_REQUEST, AuthResponseMessage.CREDENTIALS_INVALID),
+    TOKEN_REFRESH_SUCCESS(HttpStatus.OK, AuthResponseMessage.TOKEN_REFRESH_SUCCESS),
+    ACCESS_TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, AuthResponseMessage.ACCESS_TOKEN_EXPIRED),
+    
+    LOGIN_SUCCESS(HttpStatus.OK, AuthResponseMessage.LOGIN_SUCCESS),
+    LOGOUT_SUCCESS(HttpStatus.OK, AuthResponseMessage.LOGOUT_SUCCESS),
+
+    REDIS_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, AuthResponseMessage.REDIS_SERVER_EXCEPTION);
 
     private final HttpStatus httpStatus;
     private final String message;
