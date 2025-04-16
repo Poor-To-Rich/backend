3e4b2d0689b14663dc6344a6aefdf3e179d2efcc
belight2 <chlwnsgur090@gmail.com>
2025-04-16 16:36:07 +0900

    feat: 아이디, 닉네임 중복 검사 로직 추가

--- src/main/java/com/poortorich/user/controller/UserController.java
+++ src/main/java/com/poortorich/user/controller/UserController.java
@@ -3,13 +3,16 @@ package com.poortorich.user.controller;
 import com.poortorich.global.response.BaseResponse;
 import com.poortorich.user.constants.UserResponseMessages;
 import com.poortorich.user.facade.UserFacade;
+import com.poortorich.user.request.NicknameCheckRequest;
 import com.poortorich.user.request.UserRegistrationRequest;
+import com.poortorich.user.request.UsernameCheckRequest;
 import com.poortorich.user.response.enums.UserResponse;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,5 +38,15 @@ public class UserController {
     ) {
         userFacade.registerNewUser(userRegistrationRequest, profileImage);
         return BaseResponse.toResponseEntity(UserResponse.REGISTRATION_SUCCESS);
+    }
+
+    @PostMapping("/exists/username")
+    public ResponseEntity<BaseResponse> checkUsername(@RequestBody @Valid UsernameCheckRequest usernameCheckRequest) {
+        return BaseResponse.toResponseEntity(userFacade.checkUsernameAndReservation(usernameCheckRequest));
+    }
+
+    @PostMapping("/exists/nickname")
+    public ResponseEntity<BaseResponse> checkNickname(@RequestBody @Valid NicknameCheckRequest nicknameCheckRequest) {
+        return BaseResponse.toResponseEntity(userFacade.checkNicknameAndReservation(nicknameCheckRequest));
     }
 }
