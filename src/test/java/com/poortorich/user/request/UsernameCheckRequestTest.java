b3958667337b648937644aecdf498b9108f420ed
belight2 <chlwnsgur090@gmail.com>
2025-04-17 16:28:39 +0900

    test: DTO 테스트 코드 작성

--- src/test/java/com/poortorich/user/request/UsernameCheckRequestTest.java
+++ src/test/java/com/poortorich/user/request/UsernameCheckRequestTest.java
@@ -1,4 +1,25 @@
 package com.poortorich.user.request;
 
+import com.poortorich.global.testcases.TestCase;
+import com.poortorich.global.util.RequestValidTestHelper;
+import com.poortorich.user.fixture.enums.UsernameValidationCase;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
 public class UsernameCheckRequestTest {
+
+    static Stream<Arguments> usernameValidationCases() {
+        return TestCase.getAllTestCases(UsernameValidationCase.class);
+    }
+
+    @ParameterizedTest
+    @MethodSource("usernameValidationCases")
+    @DisplayName("사용자명 검증 테스트")
+    void invalidUsername_shouldFailValidation(String username, String expectedErrorMessage) {
+        RequestValidTestHelper.assertValidationMessage(new UsernameCheckRequest(username), expectedErrorMessage);
+    }
+
 }
