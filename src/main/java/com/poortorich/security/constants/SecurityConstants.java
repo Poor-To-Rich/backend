b26b783b962e6f795e52eddd044d0004c32999db
belight2 <chlwnsgur090@gmail.com>
2025-04-20 15:36:13 +0900

    feat: poortorich 도메인 추가

--- src/main/java/com/poortorich/security/constants/SecurityConstants.java
+++ src/main/java/com/poortorich/security/constants/SecurityConstants.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 public class SecurityConstants {
 
-    public static final List<String> ALLOWED_ORIGINS = List.of("http://localhost:8080", "http://localhost:5173");
+    public static final List<String> ALLOWED_ORIGINS = List.of("http://localhost:8080",
+            "http://localhost:5173",
+            "https://poortorich.site");
     public static final List<String> ALLOWED_METHOD = List.of("GET", "POST", "PUT", "DELETE");
     public static final List<String> ALLOWED_HEADERS = List.of("authorization", "content-type", "x-auth-token");
     public static final String[] PERMIT_ALL_ENDPOINTS = {
