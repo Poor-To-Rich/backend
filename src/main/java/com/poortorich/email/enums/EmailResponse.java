19c72a59607429b9dad72b90e11edeef7f9fe02c
belight2 <chlwnsgur090@gmail.com>
2025-04-24 16:01:39 +0900

    refactor: 코드 리팩터링
    추가한 상수 클래스를 사용할 수 있도록 수정
    추가한 이메일 형식 검증 애너테이션으로 수정

--- src/main/java/com/poortorich/email/enums/EmailResponse.java
+++ src/main/java/com/poortorich/email/enums/EmailResponse.java
@@ -1,5 +1,6 @@
 package com.poortorich.email.enums;
 
+import com.poortorich.email.constants.EmailResponseMessage;
 import com.poortorich.global.response.Response;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -7,40 +8,26 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum EmailResponse implements Response {
 
-    VERIFICATION_CODE_SENT("verification_code_sent", HttpStatus.OK, "인증 코드 전송 성공"),
-    INVALID_PURPOSE("invalid_purpose", HttpStatus.BAD_REQUEST, "유효하지 않은 인증 목적입니다."),
-    INVALID_VERIFICATION_CODE("invalid_verification_code", HttpStatus.BAD_REQUEST, "인증 코드가 올바르지 않습니다."),
-    VERIFICATION_CODE_EXPIRED("verification_code_expired", HttpStatus.UNAUTHORIZED, "인증 코드가 만료되었습니다."),
-    TOO_MANY_VERIFICATION_REQUESTS("too_many_verification_request", HttpStatus.TOO_MANY_REQUESTS, "인증 요청 횟수를 초과하였습니다."),
-    VERIFICATION_CODE_REQUIRED("verification_code_required", HttpStatus.BAD_REQUEST, "인증 코드를 먼저 발급받으세요."),
+    VERIFICATION_CODE_SENT(HttpStatus.OK, EmailResponseMessage.VERIFICATION_CODE_SENT),
+    INVALID_PURPOSE(HttpStatus.BAD_REQUEST, EmailResponseMessage.INVALID_PURPOSE),
+    INVALID_VERIFICATION_CODE(HttpStatus.BAD_REQUEST, EmailResponseMessage.INVALID_VERIFICATION_CODE),
+    VERIFICATION_CODE_EXPIRED(HttpStatus.UNAUTHORIZED, EmailResponseMessage.VERIFICATION_CODE_EXPIRED),
+    TOO_MANY_VERIFICATION_REQUESTS(HttpStatus.TOO_MANY_REQUESTS, EmailResponseMessage.TOO_MANY_VERIFICATION_REQUESTS),
+    VERIFICATION_CODE_REQUIRED(HttpStatus.BAD_REQUEST, EmailResponseMessage.VERIFICATION_CODE_REQUIRED),
 
-    EMAIL_VERIFICATION_SUCCESS("email_verification_success", HttpStatus.OK, "이메일 인증 성공"),
-    INVALID_EMAIL("invalid_email", HttpStatus.BAD_REQUEST, "잘못된 이메일 형식입니다."),
-    EMAIL_SEND_FAILURE("mail_send_failure", HttpStatus.INTERNAL_SERVER_ERROR, "이메일 전송에 실패했습니다."),
-    EMAIL_NOT_FOUND("mail_not_found", HttpStatus.NOT_FOUND, "존재하지 않는 이메일 주소입니다."),
-    EMAIL_NOT_VERIFIED("mail_not_verified", HttpStatus.BAD_REQUEST, "인증되지 않은 이메일입니다."),
-    TOO_MANY_REQUEST_MAIL("too_many_request_mail", HttpStatus.TOO_MANY_REQUESTS, "인증 요청 횟수를 초과하였습니다. 30분 후에 다시 시도하세요."),
+    EMAIL_VERIFICATION_SUCCESS(HttpStatus.OK, EmailResponseMessage.EMAIL_VERIFICATION_SUCCESS),
+    INVALID_EMAIL(HttpStatus.BAD_REQUEST, EmailResponseMessage.INVALID_EMAIL),
+    EMAIL_SEND_FAILURE(HttpStatus.INTERNAL_SERVER_ERROR, EmailResponseMessage.EMAIL_SEND_FAILURE),
+    EMAIL_NOT_FOUND(HttpStatus.NOT_FOUND, EmailResponseMessage.EMAIL_NOT_FOUND),
+    EMAIL_NOT_VERIFIED(HttpStatus.BAD_REQUEST, EmailResponseMessage.EMAIL_NOT_VERIFIED),
+    TOO_MANY_REQUEST_EMAIL(HttpStatus.TOO_MANY_REQUESTS, EmailResponseMessage.TOO_MANY_REQUEST_EMAIL),
 
-    TOO_MANY_CODE_RESEND_REQUESTS(
-            "too_many_code_resend_requests",
-            HttpStatus.TOO_MANY_REQUESTS,
-            "인증 코드 재발급 횟수를 초과하였습니다."
-    ),
+    TOO_MANY_CODE_RESEND_REQUESTS(HttpStatus.TOO_MANY_REQUESTS, EmailResponseMessage.TOO_MANY_CODE_RESEND_REQUESTS),
 
-    DEFAULT("default_email_exception", HttpStatus.INTERNAL_SERVER_ERROR, "내부 로직 예외 발생");
+    DEFAULT(HttpStatus.INTERNAL_SERVER_ERROR, "내부 로직 예외 발생");
 
-    private final String identifier;
     private final HttpStatus httpStatus;
     private final String message;
-
-    public static Response getResponseByIdentifier(String identifier) {
-        for (EmailResponse response : EmailResponse.values()) {
-            if (response.identifier.equals(identifier)) {
-                return response;
-            }
-        }
-        return EmailResponse.DEFAULT;
-    }
 
     @Override
     public HttpStatus getHttpStatus() {
--- src/main/java/com/poortorich/email/request/EmailVerificationRequest.java
+++ src/main/java/com/poortorich/email/request/EmailVerificationRequest.java
@@ -1,6 +1,7 @@
 package com.poortorich.email.request;
 
-import jakarta.validation.constraints.Email;
+import com.poortorich.email.constants.EmailResponseMessage;
+import com.poortorich.email.validator.annotations.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -8,10 +9,11 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public class EmailVerificationRequest {
-    @NotBlank(message = "invalid_email")
-    @Email(message = "invalid_email")
+    @NotBlank(message = EmailResponseMessage.INVALID_EMAIL)
+    @Email
     private String email;
 
-    @NotBlank(message = "invalid_purpose")
+    @NotBlank(message = EmailResponseMessage.INVALID_PURPOSE)
+    @Email
     private String purpose;
 }
