1d9df7e28c70e9427ab6bb9620dd67f72fb65c2f
belight2 <chlwnsgur090@gmail.com>
2025-04-24 16:03:12 +0900

    refactor: 코드 리팩터링

--- src/main/java/com/poortorich/email/request/VerifyEmailCodeRequest.java
+++ src/main/java/com/poortorich/email/request/VerifyEmailCodeRequest.java
@@ -1,6 +1,7 @@
 package com.poortorich.email.request;
 
-import jakarta.validation.constraints.Email;
+import com.poortorich.email.constants.EmailResponseMessage;
+import com.poortorich.email.validator.annotations.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -9,14 +10,14 @@ import org.hibernate.validator.constraints.Length;
 @Getter
 @AllArgsConstructor
 public class VerifyEmailCodeRequest {
-    @NotBlank(message = "invalid_email")
-    @Email(message = "invalid_email")
+    @NotBlank(message = EmailResponseMessage.INVALID_EMAIL)
+    @Email
     private String email;
 
-    @NotBlank(message = "invalid_purpose")
+    @NotBlank(message = EmailResponseMessage.INVALID_PURPOSE)
     private String purpose;
 
-    @NotBlank(message = "invalid_verification_code")
-    @Length(min = 6, max = 6, message = "invalid_verification_code")
+    @NotBlank(message = EmailResponseMessage.INVALID_VERIFICATION_CODE)
+    @Length(min = 6, max = 6, message = EmailResponseMessage.INVALID_VERIFICATION_CODE)
     private String verificationCode;
 }
