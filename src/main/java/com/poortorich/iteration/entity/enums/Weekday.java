07ba1f19174788e2aef15b6c270d1c9e64246f16
sseen2 <kse10023@naver.com>
2025-04-29 22:35:23 +0900

    refactor: Weekday enum 수정
    - 상수 필드 재정렬 (첫 주의 시작이 일요일이 되도록 수정)
    - toDayOfWeek, fromDayOfWeek 메서드 추가

--- src/main/java/com/poortorich/iteration/entity/enums/Weekday.java
+++ src/main/java/com/poortorich/iteration/entity/enums/Weekday.java
@@ -4,17 +4,19 @@ import com.poortorich.global.exceptions.BadRequestException;
 import com.poortorich.iteration.response.IterationResponse;
 import lombok.RequiredArgsConstructor;
 
+import java.time.DayOfWeek;
 import java.util.Objects;
 
 @RequiredArgsConstructor
 public enum Weekday {
+
+    SUNDAY("일"),
     MONDAY("월"),
     TUESDAY("화"),
     WEDNESDAY("수"),
     THURSDAY("목"),
     FRIDAY("금"),
-    SATURDAY("토"),
-    SUNDAY("일");
+    SATURDAY("토");
 
     private final String type;
 
@@ -30,5 +32,17 @@ public enum Weekday {
         }
 
         throw new BadRequestException(IterationResponse.DAY_OF_WEEK_INVALID);
+    }
+
+    public DayOfWeek toDayOfWeek() {
+        if (this.ordinal() == 0) {
+            return DayOfWeek.of(7);
+        }
+
+        return DayOfWeek.of(this.ordinal());
+    }
+
+    public static Weekday fromDayOfWeek(DayOfWeek dayOfWeek) {
+        return Weekday.values()[dayOfWeek.getValue() % 7];
     }
 }
