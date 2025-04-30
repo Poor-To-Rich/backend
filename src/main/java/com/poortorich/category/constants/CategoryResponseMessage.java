07d9827c7df7e8ede0626551cd3ecc75a193899d
sseen2 <kse10023@naver.com>
2025-04-30 22:46:19 +0900

    refactor: 카테고리 response 상수명 변경 및 메세지 상수화
    - 카테고리 response 상수명을 일관성있게 변경
    - CategoryResponseMessage 상수 클래스 추가

---
+++ src/main/java/com/poortorich/category/constants/CategoryResponseMessage.java
@@ -0,0 +1,16 @@
+package com.poortorich.category.constants;
+
+public class CategoryResponseMessage {
+
+    public static final String CREATE_CATEGORY_SUCCESS = "카테고리를 성공적으로 등록하였습니다.";
+    public static final String MODIFY_CATEGORY_SUCCESS = "카테고리를 성공적으로 편집하였습니다.";
+    public static final String DELETE_CATEGORY_SUCCESS = "카테고리를 성공적으로 삭제하였습니다.";
+
+    public static final String CATEGORY_NAME_DUPLICATE = "이미 사용중인 카테고리 이름입니다.";
+    public static final String CATEGORY_NON_EXISTENT = "존재하지 않는 카테고리입니다.";
+    public static final String CATEGORY_TYPE_INVALID = "카테고리의 타입이 적절하지 않습니다.";
+
+    public static final String DEFAULT = "알 수 없는 에러 발생";
+
+    private CategoryResponseMessage() {}
+}
