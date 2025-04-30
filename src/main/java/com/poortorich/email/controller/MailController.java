8e074de32b1d9b803496bb053fe38ed2fa818dae
belight2 <chlwnsgur090@gmail.com>
2025-04-30 18:24:12 +0900

    feat: 응답 형식 일부 삭제 및 추가

--- src/main/java/com/poortorich/email/controller/MailController.java
+++ src/main/java/com/poortorich/email/controller/MailController.java
@@ -1,25 +1,19 @@
 package com.poortorich.email.controller;
 
-import com.poortorich.email.constants.EmailResponseMessage;
+import com.poortorich.email.enums.EmailResponse;
 import com.poortorich.email.facade.EmailFacade;
-import com.poortorich.email.request.EmailBlockTimeResponse;
 import com.poortorich.email.request.EmailVerificationRequest;
 import com.poortorich.email.request.VerifyEmailCodeRequest;
-import com.poortorich.email.response.VerificationAttemptStatusResponse;
-import com.poortorich.email.response.VerificationResendCodeStatusResponse;
-import com.poortorich.email.validator.annotations.Email;
 import com.poortorich.global.response.BaseResponse;
+import com.poortorich.global.response.DataResponse;
 import com.poortorich.global.response.Response;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -35,7 +29,15 @@ public class MailController {
             @RequestBody @Valid EmailVerificationRequest emailVerificationRequest
     ) {
         Response response = emailFacade.sendVerificationCode(emailVerificationRequest);
-        return BaseResponse.toResponseEntity(response);
+
+        String email = emailVerificationRequest.getEmail();
+        if (response == EmailResponse.EMAIL_AUTH_REQUEST_BLOCKED) {
+            return DataResponse.toResponseEntity(response, emailFacade.getBlockExpired(email));
+        }
+        if (response == EmailResponse.TOO_MANY_CODE_RESEND_REQUESTS) {
+            return BaseResponse.toResponseEntity(response);
+        }
+        return DataResponse.toResponseEntity(response, emailFacade.getResendCodeAttempts(email));
     }
 
     @PostMapping("/verify")
@@ -43,42 +45,14 @@ public class MailController {
             @RequestBody @Valid VerifyEmailCodeRequest verifyEmailCodeRequest
     ) {
         Response response = emailFacade.verifyEmailCode(verifyEmailCodeRequest);
-        return BaseResponse.toResponseEntity(response);
-    }
 
-    @GetMapping("/send")
-    public ResponseEntity<VerificationResendCodeStatusResponse> getResendStatus(
-            @RequestParam
-            @Valid
-            @Email
-            @NotBlank(message = EmailResponseMessage.INVALID_EMAIL)
-            String email
-    ) {
-        VerificationResendCodeStatusResponse verificationCodeStatus = emailFacade.getResendStatus(email);
-        return ResponseEntity.ok(verificationCodeStatus);
-    }
-
-    @GetMapping("/verify")
-    public ResponseEntity<VerificationAttemptStatusResponse> getAttpemtStatus(
-            @RequestParam
-            @Valid
-            @Email
-            @NotBlank(message = EmailResponseMessage.INVALID_EMAIL)
-            String email
-    ) {
-        VerificationAttemptStatusResponse verificationAttemptStatus = emailFacade.getAttemptStatus(email);
-        return ResponseEntity.ok(verificationAttemptStatus);
-    }
-
-    @GetMapping("/block")
-    public ResponseEntity<EmailBlockTimeResponse> getBlockTime(
-            @RequestParam
-            @Valid
-            @Email
-            @NotBlank(message = EmailResponseMessage.INVALID_EMAIL)
-            String email
-    ) {
-        EmailBlockTimeResponse emailBlockTimeResponse = emailFacade.getBlockTime(email);
-        return ResponseEntity.ok(emailBlockTimeResponse);
+        String email = verifyEmailCodeRequest.getEmail();
+        if (response == EmailResponse.EMAIL_AUTH_REQUEST_BLOCKED) {
+            return DataResponse.toResponseEntity(response, emailFacade.getBlockExpired(email));
+        }
+        if (response == EmailResponse.TOO_MANY_VERIFICATION_REQUESTS) {
+            return BaseResponse.toResponseEntity(response);
+        }
+        return DataResponse.toResponseEntity(response, emailFacade.getEmailVerificationAttempts(email));
     }
 }
