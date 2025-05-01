0ad6c4400554ff32d87ce9a6e18d41126756263d
sseen2 <kse10023@naver.com>
2025-05-01 11:22:43 +0900

    refactor: 카테고리 entity 수정
    - @Column 추가
    - LocalDateTime 으로 변경

--- src/main/java/com/poortorich/category/entity/Category.java
+++ src/main/java/com/poortorich/category/entity/Category.java
@@ -1,6 +1,7 @@
 package com.poortorich.category.entity;
 
 import com.poortorich.category.entity.enums.CategoryType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
@@ -8,8 +9,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import java.sql.Timestamp;
-
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -18,6 +17,8 @@ import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.DynamicUpdate;
 import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -30,26 +31,33 @@ public class Category {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
 
     @NotNull
     @Enumerated(EnumType.STRING)
+    @Column(name = "type", nullable = false)
     private CategoryType type;
 
     @NotNull
+    @Column(name = "name", nullable = false)
     private String name;
 
     @NotNull
+    @Column(name = "color", nullable = false)
     private String color;
 
     @NotNull
+    @Column(name = "visibility", nullable = false)
     private Boolean visibility;
 
     @CreationTimestamp
-    private Timestamp createdDate;
+    @Column(name = "createdDate")
+    private LocalDateTime createdDate;
 
     @UpdateTimestamp
-    private Timestamp updatedDate;
+    @Column(name = "updatedDate")
+    private LocalDateTime updatedDate;
 
     // User 외래키
 
