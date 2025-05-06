9566a2030198783e2a0856f6fc20d16d15d5b22e
sseen2 <kse10023@naver.com>
2025-05-06 21:48:44 +0900

    refactor: category repository 반환값 수정

--- src/main/java/com/poortorich/category/repository/CategoryRepository.java
+++ src/main/java/com/poortorich/category/repository/CategoryRepository.java
@@ -6,6 +6,7 @@ import com.poortorich.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -15,5 +16,5 @@ public interface CategoryRepository extends JpaRepository<Category, Long> {
 
     Optional<Category> findByNameAndUser(String name, User user);
 
-    Optional<Category> findByTypeAndUser(CategoryType type, User user);
+    List<Category> findByTypeAndUser(CategoryType type, User user);
 }
