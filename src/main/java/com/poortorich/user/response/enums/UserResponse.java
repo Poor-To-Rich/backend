c9069b3821229bf1880a3ce05fefac03f491d2af
belight2 <chlwnsgur090@gmail.com>
2025-05-11 20:56:01 +0900

    feat: 비밀번호 변경 응답 메시지 추가

--- src/main/java/com/poortorich/user/response/enums/UserResponse.java
+++ src/main/java/com/poortorich/user/response/enums/UserResponse.java
@@ -25,7 +25,9 @@ public enum UserResponse implements Response {
     GENDER_INVALID(HttpStatus.BAD_REQUEST, UserResponseMessages.GENDER_INVALID, "gender"),
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, UserResponseMessages.USER_NOT_FOUND, null),
     USER_DETAIL_FIND_SUCCESS(HttpStatus.OK, UserResponseMessages.USER_DETAIL_FIND_SUCCESS, null),
-    USER_EMAIL_FIND_SUCCESS(HttpStatus.OK, UserResponseMessages.USER_EMAIL_FIND_SUCCESS, null);
+    USER_EMAIL_FIND_SUCCESS(HttpStatus.OK, UserResponseMessages.USER_EMAIL_FIND_SUCCESS, null),
+    PASSWORD_UPDATE_SUCCESS(HttpStatus.OK, UserResponseMessages.PASSWORD_UPDATE_SUCCESS, null),
+    USER_PROFILE_UPDATE_SUCCESS(HttpStatus.OK, UserResponseMessages.USER_PROFILE_UPDATE_SUCCESS, null);
 
     private final HttpStatus httpStatus;
     private final String message;
