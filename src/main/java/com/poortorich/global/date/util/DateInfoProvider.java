1201db6e55ab8f1e667f381ff5fdb7326bf7179e
belight2 <chlwnsgur090@gmail.com>
2025-05-14 18:42:04 +0900

    feat: 년, 월, 일 정보를 제공하는 유틸객체 추가

--- src/main/java/com/poortorich/global/date/util/DateInfoProvider.java
+++ src/main/java/com/poortorich/global/date/util/DateInfoProvider.java
@@ -1,24 +1,59 @@
-package com.poortorich.global.util;
+package com.poortorich.global.date.util;
 
-import com.poortorich.global.domain.MonthInformation;
-import com.poortorich.global.domain.WeekInformation;
-import com.poortorich.global.domain.YearInformation;
+import com.poortorich.global.date.constants.DateConstants;
+import com.poortorich.global.date.domain.MonthInformation;
+import com.poortorich.global.date.domain.WeekInformation;
+import com.poortorich.global.date.domain.YearInformation;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.Month;
 import java.time.Year;
 import java.time.YearMonth;
+import java.time.temporal.TemporalAdjusters;
+import java.util.ArrayList;
+import java.util.EnumMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
 
 public class DateInfoProvider {
 
     public static YearInformation getYearInformation(Year year) {
+        Map<Month, MonthInformation> months = new EnumMap<>(Month.class);
+        Stream.of(Month.values())
+                .forEach(month -> months.put(month, getMonthInformation(year.atMonth(month))));
 
+        return YearInformation.builder()
+                .year(year)
+                .startDate(year.atDay(DateConstants.ONE_DAY))
+                .endDate(year.atMonth(Month.DECEMBER).atEndOfMonth())
+                .months(months)
+                .build();
     }
 
     public static MonthInformation getMonthInformation(YearMonth yearMonth) {
-
+        return MonthInformation.builder()
+                .yearMonth(yearMonth)
+                .startDate(yearMonth.atDay(DateConstants.ONE_DAY))
+                .endDate(yearMonth.atEndOfMonth())
+                .weeks(DateInfoProvider.getWeekInformation(yearMonth))
+                .build();
     }
 
     public static List<WeekInformation> getWeekInformation(YearMonth yearMonth) {
-        int lengthOfMonth = yearMonth.lengthOfMonth();
+        List<WeekInformation> weeks = new ArrayList<>();
+        LocalDate weekStart = yearMonth.atDay(DateConstants.ONE_DAY)
+                .with(TemporalAdjusters.previousOrSame(DayOfWeek.SUNDAY));
 
+        while (!weekStart.isAfter(yearMonth.atEndOfMonth())) {
+            LocalDate weekEnd = weekStart.with(TemporalAdjusters.next(DayOfWeek.SATURDAY));
+            weeks.add(WeekInformation.builder()
+                    .startDate(weekStart)
+                    .endDate(weekEnd)
+                    .build());
+            weekStart = weekEnd.plusDays(DateConstants.ONE_DAY);
+        }
+
+        return weeks;
     }
 }
