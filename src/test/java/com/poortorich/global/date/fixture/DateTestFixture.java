35ced320437b46496378e2818c1b66e43363d49c
belight2 <chlwnsgur090@gmail.com>
2025-05-14 18:42:43 +0900

    test: DateInfoProvider 테스트 코드 추가

--- src/test/java/com/poortorich/global/date/fixture/DateTestFixture.java
+++ src/test/java/com/poortorich/global/date/fixture/DateTestFixture.java
@@ -1,4 +1,28 @@
 package com.poortorich.global.date.fixture;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.Year;
+import java.time.YearMonth;
+
 public class DateTestFixture {
+
+    public static final Year TEST_YEAR = Year.of(2025);
+
+    public static final YearMonth MAY_2025 = YearMonth.of(2025, 5);
+
+    public static final LocalDate MAY_START = MAY_2025.atDay(1);
+    public static final LocalDate MAY_END = MAY_2025.atEndOfMonth();
+
+    public static final LocalDate YEAR_START = TEST_YEAR.atDay(1);
+    public static final LocalDate YEAR_END = TEST_YEAR.atMonth(12).atEndOfMonth();
+
+    public static final DayOfWeek WEEK_START_DAY = DayOfWeek.SUNDAY;
+    public static final DayOfWeek WEEK_END_DAY = DayOfWeek.SATURDAY;
+
+    public static final LocalDate MAY_2025_WEEK_FIRST_START = LocalDate.of(2025, 4, 27);
+    public static final LocalDate MAY_2025_WEEK_FIRST_END = LocalDate.of(2025, 5, 3);
+
+    public static final LocalDate MAY_2025_WEEK_LAST_START = LocalDate.of(2025, 5, 25);
+    public static final LocalDate MAY_2025_WEEK_LAST_END = LocalDate.of(2025, 5, 31);
 }
--- src/test/java/com/poortorich/global/date/util/DateInfoProviderTest.java
+++ src/test/java/com/poortorich/global/date/util/DateInfoProviderTest.java
@@ -1,4 +1,46 @@
 package com.poortorich.global.date.util;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.poortorich.global.date.domain.MonthInformation;
+import com.poortorich.global.date.domain.WeekInformation;
+import com.poortorich.global.date.domain.YearInformation;
+import com.poortorich.global.date.fixture.DateTestFixture;
+import java.time.Month;
+import java.util.List;
+import java.util.Map;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class DateInfoProviderTest {
+
+    @Test
+    @DisplayName("해당 연도의 정보를 반환받을 때, 유효한 구조로 반환이 되는지 확인")
+    void getYearInformation_whenGivenYear_shouldReturnValidStructure() {
+        YearInformation result = DateInfoProvider.getYearInformation(DateTestFixture.TEST_YEAR);
+        assertThat(result).isNotNull();
+        assertThat(result.getYear()).isEqualTo(DateTestFixture.TEST_YEAR);
+        assertThat(result.getStartDate()).isEqualTo(DateTestFixture.YEAR_START);
+        assertThat(result.getEndDate()).isEqualTo(DateTestFixture.YEAR_END);
+
+        Map<Month, MonthInformation> months = result.getMonths();
+        assertThat(months).hasSize(12);
+        assertThat(months.keySet()).containsExactlyInAnyOrder(Month.values());
+
+        MonthInformation mayInfo = months.get(Month.MAY);
+        assertThat(mayInfo.getYearMonth()).isEqualTo(DateTestFixture.MAY_2025);
+        assertThat(mayInfo.getStartDate()).isEqualTo(DateTestFixture.MAY_START);
+        assertThat(mayInfo.getEndDate()).isEqualTo(DateTestFixture.MAY_END);
+
+        List<WeekInformation> weeksInMay = mayInfo.getWeeks();
+        assertThat(weeksInMay).isNotEmpty();
+
+        WeekInformation firstWeek = weeksInMay.getFirst();
+        assertThat(firstWeek.getStartDate()).isEqualTo(DateTestFixture.MAY_2025_WEEK_FIRST_START);
+        assertThat(firstWeek.getEndDate()).isEqualTo(DateTestFixture.MAY_2025_WEEK_FIRST_END);
+
+        WeekInformation endWeek = weeksInMay.getLast();
+        assertThat(endWeek.getStartDate()).isEqualTo(DateTestFixture.MAY_2025_WEEK_LAST_START);
+        assertThat(endWeek.getEndDate()).isEqualTo(DateTestFixture.MAY_2025_WEEK_LAST_END);
+    }
 }
