6ead0c4f15f8f7de5f0195db2482228deb508639
belight2 <chlwnsgur090@gmail.com>
2025-05-15 14:52:18 +0900

    feat: 1년 기준 첫날 상수 추가

--- src/main/java/com/poortorich/global/date/constants/DateConstants.java
+++ src/main/java/com/poortorich/global/date/constants/DateConstants.java
@@ -2,9 +2,7 @@ package com.poortorich.global.date.constants;
 
 public class DateConstants {
 
+    public static final int FIRTH_DAY_OF_YEAR = 1;
     public static final int FIRST_DAY_OF_MONTH = 1;
-    public static final int DAYS_IN_WEEK = 7;
-    public static final int FIRST_WEEK = 1;
     public static final int ONE_DAY = 1;
-    public static final int SIX_DAYS = 6;
 }
