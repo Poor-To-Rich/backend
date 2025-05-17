79f1a5ccb0f5922acd79bb51ab686d8a5f926c80
belight2 <chlwnsgur090@gmail.com>
2025-05-17 17:10:54 +0900

    feat: GlobalResponse 추가
    여러 기능에서 사용할 동일한 Response를 관리할 객체 추가

--- src/main/java/com/poortorich/global/response/enums/GlobalResponse.java
+++ src/main/java/com/poortorich/global/response/enums/GlobalResponse.java
@@ -1,4 +1,30 @@
 package com.poortorich.global.response.enums;
 
-public class GlobalResponse {
+import com.poortorich.global.response.Response;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@AllArgsConstructor
+public enum GlobalResponse implements Response {
+
+    INTERNAL_SERVER_EXCEPTION(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부에 문제 발생가 발생했습니다. 다시 시도해주세요.", null);
+
+    private final HttpStatus httpStatus;
+    private final String message;
+    private final String field;
+
+    @Override
+    public HttpStatus getHttpStatus() {
+        return httpStatus;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+
+    @Override
+    public String getField() {
+        return field;
+    }
 }
