ac0d3a29d718cab1954aa90f2e55bfdb207ce6e2
belight2 <chlwnsgur090@gmail.com>
2025-05-26 13:11:07 +0900

    feat: 총액 및 저축액 조회 로직 추가

--- src/main/java/com/poortorich/chart/facade/ChartFacade.java
+++ src/main/java/com/poortorich/chart/facade/ChartFacade.java
@@ -1,8 +1,10 @@
 package com.poortorich.chart.facade;
 
 import com.poortorich.category.domain.model.enums.DefaultExpenseCategory;
+import com.poortorich.category.entity.Category;
 import com.poortorich.category.service.CategoryService;
-import com.poortorich.chart.response.TotalExpenseAndSavingResponse;
+import com.poortorich.chart.response.TotalAmountAndSavingResponse;
+import com.poortorich.chart.service.ChartService;
 import com.poortorich.expense.entity.Expense;
 import com.poortorich.expense.service.ExpenseService;
 import com.poortorich.global.date.domain.DateInfo;
@@ -10,7 +12,6 @@ import com.poortorich.global.date.util.DateInfoProvider;
 import com.poortorich.user.entity.User;
 import com.poortorich.user.service.UserService;
 import java.util.List;
-import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -18,35 +19,24 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class ChartFacade {
 
+    private final ChartService chartService;
     private final UserService userService;
     private final CategoryService categoryService;
     private final ExpenseService expenseService;
 
-    public TotalExpenseAndSavingResponse getTotalExpenseAmountAndSaving(String username, String date) {
+    public TotalAmountAndSavingResponse getTotalExpenseAmountAndSaving(String username, String date) {
         User user = userService.findUserByUsername(username);
         DateInfo dateInfo = DateInfoProvider.getDateInfo(date);
 
         List<Expense> userExpenses = expenseService.getExpensesBetweenDates(
                 user, dateInfo.getStartDate(), dateInfo.getEndDate()
         );
-        long savingCategoryId = categoryService.findCategoryByName(
-                        DefaultExpenseCategory.SAVINGS_INVESTMENT.name(), user)
-                .getId();
 
-        long totalExpenseAmount = 0;
-        long totalSavingAmount = 0;
-        for (Expense expense : userExpenses) {
-            if (Objects.equals(expense.getCategory().getName(), DefaultExpenseCategory.SAVINGS_INVESTMENT.name())) {
-                totalSavingAmount += expense.getCost();
-                continue;
-            }
-            totalExpenseAmount += expense.getCost();
-        }
+        Category savingCategory = categoryService.findCategoryByName(
+                DefaultExpenseCategory.SAVINGS_INVESTMENT.name(),
+                user
+        );
 
-        return TotalExpenseAndSavingResponse.builder()
-                .savingCategoryId(savingCategoryId)
-                .totalExpenseAmount(totalExpenseAmount)
-                .totalSavingAmount(totalSavingAmount)
-                .build();
+        return chartService.getTotalExpenseAndSavings(userExpenses, savingCategory);
     }
 }
