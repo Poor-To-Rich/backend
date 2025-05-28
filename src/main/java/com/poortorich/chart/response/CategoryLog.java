df44d9a1262d2315fbcf394217e7321e2393858e
belight2 <chlwnsgur090@gmail.com>
2025-05-28 13:45:31 +0900

    feat: 응답 DTO 추가

--- src/main/java/com/poortorich/chart/response/CategoryLog.java
+++ src/main/java/com/poortorich/chart/response/CategoryLog.java
@@ -1,4 +1,14 @@
 package com.poortorich.chart.response;
 
+import java.util.List;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class CategoryLog {
+
+    private String date;
+    private Long countOfTransactions;
+    private List<TransactionRecord> transactions;
 }
--- src/main/java/com/poortorich/chart/response/CategorySectionResponse.java
+++ src/main/java/com/poortorich/chart/response/CategorySectionResponse.java
@@ -1,4 +1,14 @@
 package com.poortorich.chart.response;
 
+import java.util.List;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class CategorySectionResponse {
+    private Long countOfLogs;
+    private Boolean hasNext;
+    private String nextCursor;
+    private List<CategoryLog> categoryLogs;
 }
