50e03a2d371b02b5dec830eb4c82d50567563f19
belight2 <chlwnsgur090@gmail.com>
2025-06-01 14:22:41 +0900

    feat: CategoryLog 빌드 로직 추가

--- src/main/java/com/poortorich/chart/service/ChartService.java
+++ src/main/java/com/poortorich/chart/service/ChartService.java
@@ -1,7 +1,10 @@
 package com.poortorich.chart.service;
 
 import com.poortorich.category.entity.Category;
+import com.poortorich.chart.response.CategoryLog;
 import com.poortorich.chart.response.TotalAmountAndSavingResponse;
+import com.poortorich.chart.response.TransactionRecord;
+import com.poortorich.chart.util.TransactionUtil;
 import com.poortorich.expense.entity.Expense;
 import com.poortorich.expense.util.ExpenseCostExtractor;
 import com.poortorich.global.statistics.util.StatCalculator;
@@ -27,5 +30,20 @@ public class ChartService {
                 .totalAmount(totalExpense)
                 .totalSaving(totalSavings)
                 .build();
+    }
+
+    public List<CategoryLog> getCategoryLogs(List<Expense> expenses) {
+        return TransactionUtil.groupExpensesByDate(expenses)
+                .stream()
+                .map(expensesOnDate -> {
+                    List<TransactionRecord> transactions = TransactionUtil.mapToTransactionRecord(expensesOnDate);
+
+                    return CategoryLog.builder()
+                            .date(expensesOnDate.getFirst().getExpenseDate().toString())
+                            .countOfTransactions((long) transactions.size())
+                            .transactions(transactions)
+                            .build();
+                })
+                .toList();
     }
 }
