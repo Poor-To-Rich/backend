d783bcc25d09d12169eb0ba743d73ecd5a108a69
belight2 <chlwnsgur090@gmail.com>
2025-06-01 20:06:12 +0900

    feat: Expense, Income 빌더 추가

--- src/main/java/com/poortorich/accountbook/util/AccountBookBuilder.java
+++ src/main/java/com/poortorich/accountbook/util/AccountBookBuilder.java
@@ -1,4 +1,50 @@
 package com.poortorich.accountbook.util;
 
+import com.poortorich.accountbook.entity.AccountBook;
+import com.poortorich.accountbook.enums.AccountBookType;
+import com.poortorich.accountbook.request.AccountBookRequest;
+import com.poortorich.category.entity.Category;
+import com.poortorich.expense.entity.Expense;
+import com.poortorich.expense.request.ExpenseRequest;
+import com.poortorich.income.entity.Income;
+import com.poortorich.income.request.IncomeRequest;
+import com.poortorich.user.entity.User;
+
 public class AccountBookBuilder {
+
+    public static AccountBook buildEntity(
+            User user,
+            Category category,
+            AccountBookRequest accountBookRequest,
+            AccountBookType type) {
+        return switch (type) {
+            case EXPENSE -> AccountBookBuilder.buildExpense(user, category, (ExpenseRequest) accountBookRequest);
+            case INCOME -> AccountBookBuilder.buildIncome(user, category, (IncomeRequest) accountBookRequest);
+        };
+    }
+
+    private static AccountBook buildExpense(User user, Category category, ExpenseRequest expenseRequest) {
+        return Expense.builder()
+                .expenseDate(expenseRequest.parseDate())
+                .category(category)
+                .title(expenseRequest.trimTitle())
+                .cost(expenseRequest.getCost())
+                .paymentMethod(expenseRequest.parsePaymentMethod())
+                .memo(expenseRequest.getMemo())
+                .iterationType(expenseRequest.parseIterationType())
+                .user(user)
+                .build();
+    }
+
+    private static AccountBook buildIncome(User user, Category category, IncomeRequest incomeRequest) {
+        return Income.builder()
+                .incomeDate(incomeRequest.parseDate())
+                .category(category)
+                .title(incomeRequest.trimTitle())
+                .cost(incomeRequest.getCost())
+                .memo(incomeRequest.getMemo())
+                .iterationType(incomeRequest.parseIterationType())
+                .user(user)
+                .build();
+    }
 }
