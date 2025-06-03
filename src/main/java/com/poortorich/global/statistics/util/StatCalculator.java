cad1db01bb353919eb3dc95f741c25e2505f1313
belight2 <chlwnsgur090@gmail.com>
2025-06-03 14:04:50 +0900

    feat: 로직 안정성 추가
    빈 리스트로 로직 수행시 아래와 같은 예외 발생
    - 인덱스 범위 예외
    - 0 나누기 예외
    
    따라서 빈리스트인지 검증하는 로직 추가

--- src/main/java/com/poortorich/global/statistics/util/StatCalculator.java
+++ src/main/java/com/poortorich/global/statistics/util/StatCalculator.java
@@ -3,8 +3,10 @@ package com.poortorich.global.statistics.util;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -15,19 +17,38 @@ public class StatCalculator {
     private static final int ROUNDING_SCALE = 1;
 
     public static BigDecimal calculateSum(List<Long> numbers) {
+        if (Objects.isNull(numbers) || numbers.isEmpty()) {
+            return BigDecimal.ZERO;
+        }
+
         return numbers.stream()
+                .filter(Objects::nonNull)
                 .map(BigDecimal::valueOf)
                 .reduce(BigDecimal.ZERO, BigDecimal::add);
     }
 
     public static BigDecimal calculateAverage(List<Long> numbers) {
+        if (Objects.isNull(numbers) || numbers.isEmpty()) {
+            return BigDecimal.ZERO;
+        }
+
         return StatCalculator.calculateSum(numbers)
                 .divide(BigDecimal.valueOf(numbers.size()), DIVISION_SCALE, RoundingMode.HALF_UP)
                 .setScale(ROUNDING_SCALE, RoundingMode.HALF_UP);
     }
 
     public static List<BigDecimal> calculatePercentages(List<Long> numbers) {
+        if (Objects.isNull(numbers) || numbers.isEmpty()) {
+            return Collections.emptyList();
+        }
+
         BigDecimal sum = StatCalculator.calculateSum(numbers);
+
+        if (sum.compareTo(BigDecimal.ZERO) == 0) {
+            return numbers.stream()
+                    .map(number -> BigDecimal.ZERO)
+                    .toList();
+        }
 
         return StatCalculator.adjustPercentages(numbers.stream()
                 .map(BigDecimal::valueOf)
