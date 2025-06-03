5d80c411ccdd40a5794c60e49be30840acb3a31d
sseen2 <kse10023@naver.com>
2025-06-03 15:30:08 +0900

    feat: 수입 반복데이터 추가 기능 구현

--- src/main/java/com/poortorich/accountbook/util/AccountBookBuilder.java
+++ src/main/java/com/poortorich/accountbook/util/AccountBookBuilder.java
@@ -10,6 +10,8 @@ import com.poortorich.income.entity.Income;
 import com.poortorich.income.request.IncomeRequest;
 import com.poortorich.user.entity.User;
 
+import java.time.LocalDate;
+
 public class AccountBookBuilder {
 
     public static AccountBook buildEntity(
@@ -44,6 +46,43 @@ public class AccountBookBuilder {
                 .cost(incomeRequest.getCost())
                 .memo(incomeRequest.getMemo())
                 .iterationType(incomeRequest.parseIterationType())
+                .user(user)
+                .build();
+    }
+
+    public static AccountBook buildEntity(
+            User user,
+            LocalDate date,
+            AccountBook originalAccountBook,
+            AccountBookType type
+    ) {
+        return switch (type) {
+            case EXPENSE -> AccountBookBuilder.buildIterationExpense(user, date, (Expense) originalAccountBook);
+            case INCOME -> AccountBookBuilder.buildIterationIncome(user, date, (Income) originalAccountBook);
+        };
+    }
+
+    private static AccountBook buildIterationExpense(User user, LocalDate date, Expense originalExpense) {
+        return Expense.builder()
+                .expenseDate(date)
+                .title(originalExpense.getTitle())
+                .cost(originalExpense.getCost())
+                .paymentMethod(originalExpense.getPaymentMethod())
+                .memo(originalExpense.getMemo())
+                .iterationType(originalExpense.getIterationType())
+                .category(originalExpense.getCategory())
+                .user(user)
+                .build();
+    }
+
+    private static AccountBook buildIterationIncome(User user, LocalDate date, Income originalIncome) {
+        return Income.builder()
+                .incomeDate(date)
+                .title(originalIncome.getTitle())
+                .cost(originalIncome.getCost())
+                .memo(originalIncome.getMemo())
+                .iterationType(originalIncome.getIterationType())
+                .category(originalIncome.getCategory())
                 .user(user)
                 .build();
     }
--- src/main/java/com/poortorich/income/facade/IncomeFacade.java
+++ src/main/java/com/poortorich/income/facade/IncomeFacade.java
@@ -1,6 +1,7 @@
 package com.poortorich.income.facade;
 
 import com.poortorich.accountbook.entity.AccountBook;
+import com.poortorich.accountbook.entity.enums.IterationType;
 import com.poortorich.accountbook.enums.AccountBookType;
 import com.poortorich.accountbook.service.AccountBookService;
 import com.poortorich.category.entity.Category;
@@ -8,12 +9,14 @@ import com.poortorich.category.service.CategoryService;
 import com.poortorich.global.response.Response;
 import com.poortorich.income.request.IncomeRequest;
 import com.poortorich.income.response.enums.IncomeResponse;
-import com.poortorich.income.service.IncomeService;
+import com.poortorich.iteration.service.IterationService;
 import com.poortorich.user.entity.User;
 import com.poortorich.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -23,18 +26,26 @@ public class IncomeFacade {
 
     private final UserService userService;
     private final CategoryService categoryService;
-    private final IncomeService incomeService;
-
-    // Change Service Layer
     private final AccountBookService accountBookService;
+    private final IterationService iterationService;
 
     @Transactional
     public Response createIncome(String username, IncomeRequest incomeRequest) {
         User user = userService.findUserByUsername(username);
         Category category = categoryService.findCategoryByName(incomeRequest.getCategoryName(), user);
-        AccountBook accountBook = accountBookService.create(user, category, incomeRequest, accountBookType);
-//        Income income = incomeService.create(incomeRequest, category, user);
+        AccountBook income = accountBookService.create(user, category, incomeRequest, accountBookType);
+
+        if (income.getIterationType() != IterationType.DEFAULT) {
+            createIteration(user, incomeRequest, income);
+        }
 
         return IncomeResponse.CREATE_INCOME_SUCCESS;
+    }
+
+    public void createIteration(User user, IncomeRequest incomeRequest, AccountBook income) {
+        List<AccountBook> iterationExpenses
+                = iterationService.createIterations(user, incomeRequest.getCustomIteration(), income, accountBookType);
+        List<AccountBook> savedExpenses = accountBookService.createAccountBookAll(iterationExpenses, accountBookType);
+        iterationService.createIterationInfo(user, incomeRequest, income, savedExpenses, accountBookType);
     }
 }
