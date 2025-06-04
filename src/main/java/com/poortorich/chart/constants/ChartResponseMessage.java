8680b932e6885b9385d39a108d73f45845a87ed5
belight2 <chlwnsgur090@gmail.com>
2025-06-04 18:11:58 +0900

    feat: 응답 메시지 추가

--- src/main/java/com/poortorich/chart/constants/ChartResponseMessage.java
+++ src/main/java/com/poortorich/chart/constants/ChartResponseMessage.java
@@ -5,6 +5,7 @@ public class ChartResponseMessage {
     public static final String GET_TOTAL_EXPENSE_AND_SAVINGS_SUCCESS = "지출 및 저축 금액 조회 성공";
     public static final String GET_CATEGORY_SECTION_SUCCESS = "카테고리 구간 내역 조회 성공";
     public static final String GET_CATEGORY_CHART_SUCCESS = "카테고리별 비율 및 총 금액 조회 성공";
+    public static final String GET_EXPENSE_BAR_SUCCESS = "지출 막대그래프 조회 성공";
 
     private ChartResponseMessage() {
     }
