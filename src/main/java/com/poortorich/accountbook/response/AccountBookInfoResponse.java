b4c6f35b03b13d49b364f0185557d920bf10c5df
sseen2 <kse10023@naver.com>
2025-06-04 10:57:53 +0900

    feat: 가계부 반복 데이터 정보 response 추가

---
+++ src/main/java/com/poortorich/accountbook/response/AccountBookInfoResponse.java
@@ -0,0 +1,20 @@
+package com.poortorich.accountbook.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AccountBookInfoResponse {
+
+    private Long id;
+    private String color;
+    private String categoryName;
+    private String title;
+    private String type;
+    private Long cost;
+}
