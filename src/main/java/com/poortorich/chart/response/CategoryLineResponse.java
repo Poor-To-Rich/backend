47aa8bfce92a15bec70396316481132a424643cb
belight2 <chlwnsgur090@gmail.com>
2025-06-05 14:49:10 +0900

    feat: 카테고리 꺾은선 DTO 추가

--- src/main/java/com/poortorich/chart/response/CategoryLineResponse.java
+++ src/main/java/com/poortorich/chart/response/CategoryLineResponse.java
@@ -1,4 +1,14 @@
 package com.poortorich.chart.response;
 
+import java.util.List;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class CategoryLineResponse {
+
+    private String period;
+    private Long totalAmounts;
+    private List<PeriodTotal> weeklyAmounts;
 }
