28ba8533779f150d9a79109396bba08990fb92d7
belight2 <chlwnsgur090@gmail.com>
2025-06-06 14:50:29 +0900

    feat: 반복 규칙 데이터 제거 추가

--- src/main/java/com/poortorich/user/service/UserResetService.java
+++ src/main/java/com/poortorich/user/service/UserResetService.java
@@ -1,13 +1,19 @@
 package com.poortorich.user.service;
 
+import com.poortorich.accountbook.enums.AccountBookType;
 import com.poortorich.accountbook.repository.AccountBookRepository;
 import com.poortorich.category.entity.enums.CategoryType;
 import com.poortorich.category.repository.CategoryRepository;
 import com.poortorich.global.exceptions.InternalServerErrorException;
 import com.poortorich.global.response.enums.GlobalResponse;
+import com.poortorich.iteration.entity.Iteration;
+import com.poortorich.iteration.repository.IterationInfoRepository;
 import com.poortorich.iteration.repository.IterationRepository;
 import com.poortorich.user.entity.User;
-import com.poortorich.user.repository.UserRepository;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -19,20 +25,25 @@ import org.springframework.transaction.annotation.Transactional;
 @Slf4j
 public class UserResetService {
 
-    private final UserRepository userRepository;
     private final IterationRepository iterationRepository;
     private final AccountBookRepository accountBookRepository;
     private final CategoryRepository categoryRepository;
+    private final IterationInfoRepository iterationInfoRepository;
 
     public void deleteUserAllData(User user) {
         log.info("Starting Date Reset");
         log.info("데이터 삭제는 DB 제약 사항 순서에 맞추어 진행됩니다.");
         try {
+            Set<Long> iterationInfoIds = getDeletedIterationInfo(user);
+
             log.info("[user ID: {}] 반복 데이터 삭제", user.getId());
             iterationRepository.deleteAllIterationByUser(user);
 
-            log.info("[user Id: {}] 지출/수입 데이터 삭제", user.getId());
+            log.info("[user ID: {}] 지출/수입 데이터 삭제", user.getId());
             accountBookRepository.deleteAllAccountBooksByUser(user);
+
+            log.info("[user ID: {}] 반복 규칙 정보 삭제", user.getId());
+            iterationInfoRepository.deleteAllById(iterationInfoIds);
 
             log.info("[user ID: {}] 커스텀 카테고리 삭제", user.getId());
             categoryRepository.deleteByUserAndType(user, CategoryType.CUSTOM_EXPENSE);
@@ -43,5 +54,14 @@ public class UserResetService {
             log.error("모든 데이터 초기화 중 예외가 발생: {}", user.getId(), e);
             throw new InternalServerErrorException(GlobalResponse.INTERNAL_SERVER_EXCEPTION);
         }
+    }
+
+    private Set<Long> getDeletedIterationInfo(User user) {
+        List<Iteration> iterationExpenses = iterationRepository.findAllByUser(user, AccountBookType.EXPENSE);
+        List<Iteration> iterationIncomes = iterationRepository.findAllByUser(user, AccountBookType.INCOME);
+
+        return Stream.concat(iterationExpenses.stream(), iterationIncomes.stream())
+                .map(Iteration::getId)
+                .collect(Collectors.toSet());
     }
 }
