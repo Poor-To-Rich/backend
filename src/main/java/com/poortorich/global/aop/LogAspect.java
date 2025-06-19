b0c12597caa6ee6d5ecce2a65815184da62c9c8d
sseen2 <kse10023@naver.com>
2025-06-19 15:19:39 +0900

    feat: AOP 추가

---
+++ src/main/java/com/poortorich/global/aop/LogAspect.java
@@ -0,0 +1,82 @@
+package com.poortorich.global.aop;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+@Aspect
+@Slf4j
+@Component
+public class LogAspect {
+
+    @Pointcut("execution(* com.poortorich..*.*(..)) && " +
+            "!execution(* com.poortorich.global..*(..)) && " +
+            "!execution(* com.poortorich.security..*(..))")
+    public void all() {
+    }
+
+    @Pointcut("execution(* com.poortorich..controller..*(..))")
+    public void controller() {
+    }
+
+    @Around("all()")
+    public Object logging(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        try {
+            return joinPoint.proceed();
+        } finally {
+            long end = System.currentTimeMillis();
+            long timeInMs = end - start;
+            log.info("{} | time = {}ms", joinPoint.getSignature(), timeInMs);
+        }
+    }
+
+    @Around("controller()")
+    public Object loggingBefore(ProceedingJoinPoint joinPoint) throws Throwable {
+        HttpServletRequest request =
+                ((ServletRequestAttributes) Objects.requireNonNull(RequestContextHolder.getRequestAttributes()))
+                        .getRequest();
+
+        String controllerName = joinPoint.getSignature().getDeclaringType().getName();
+        String methodName = joinPoint.getSignature().getName();
+        Map<String, Object> params = new HashMap<>();
+
+        try {
+            params.put("controller", controllerName);
+            params.put("method", methodName);
+            params.put("params", getParams(request));
+            params.put("log_time", System.currentTimeMillis());
+            params.put("request_uri", request.getRequestURI());
+            params.put("http_method", request.getMethod());
+        } catch (Exception e) {
+            log.error("LoggerAspect error", e);
+        }
+
+        log.info("[{}] {}", params.get("http_method"), params.get("request_uri"));
+        log.info("method: {}.{}", params.get("controller") ,params.get("method"));
+        log.info("params: {}", params.get("params"));
+
+        return joinPoint.proceed();
+    }
+
+    private Object getParams(HttpServletRequest request) {
+        Map<String, String> paramMap = new HashMap<>();
+        Enumeration<String> params = request.getParameterNames();
+        while (params.hasMoreElements()) {
+            String param = params.nextElement();
+            paramMap.put(param.replaceAll("\\.", "-"), request.getParameter(param));
+        }
+        return paramMap;
+    }
+}
