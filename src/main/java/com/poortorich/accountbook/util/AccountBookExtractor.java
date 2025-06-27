73126f4e485811db791c2cbb8fdea1c403c4ac28
belight2 <chlwnsgur090@gmail.com>
2025-06-27 12:08:30 +0900

    feat: AccountBook 추출기 추가

--- src/main/java/com/poortorich/accountbook/util/AccountBookExtractor.java
+++ src/main/java/com/poortorich/accountbook/util/AccountBookExtractor.java
@@ -1,4 +1,24 @@
 package com.poortorich.accountbook.util;
 
+import com.poortorich.accountbook.entity.AccountBook;
+import com.poortorich.category.entity.Category;
+import java.util.List;
+import java.util.Objects;
+
 public class AccountBookExtractor {
+
+    private AccountBookExtractor() {
+    }
+
+    public static List<AccountBook> extractByCategory(List<AccountBook> accountBooks, Category category) {
+        return accountBooks.stream()
+                .filter(accountBook -> Objects.equals(accountBook.getCategory(), category))
+                .toList();
+    }
+
+    public static List<AccountBook> extractExcludingCategory(List<AccountBook> accountBooks, Category category) {
+        return accountBooks.stream()
+                .filter(accountBook -> !Objects.equals(accountBook.getCategory(), category))
+                .toList();
+    }
 }
