f13da1ad2becf91ecb038c3bef831318e12f1250
belight2 <chlwnsgur090@gmail.com>
2025-06-27 12:15:52 +0900

    refactor: ChartService 리팩터링

--- src/main/java/com/poortorich/chart/aggregator/ChartDataAggregator.java
+++ src/main/java/com/poortorich/chart/aggregator/ChartDataAggregator.java
@@ -1,4 +1,60 @@
 package com.poortorich.chart.aggregator;
 
+import com.poortorich.accountbook.entity.AccountBook;
+import com.poortorich.accountbook.util.AccountBookCalculator;
+import com.poortorich.chart.constants.ChartConstants;
+import com.poortorich.chart.response.CategoryChart;
+import com.poortorich.chart.util.AmountFormatter;
+import com.poortorich.global.date.domain.DateInfo;
+import io.jsonwebtoken.lang.Objects;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import org.springframework.stereotype.Component;
+
+@Component
 public class ChartDataAggregator {
+
+    public Map<String, Double> getAggregatedDataFromCategoryChart(List<CategoryChart> categoryCharts) {
+        if (Objects.isEmpty(categoryCharts)) {
+            return Map.of(ChartConstants.DUMMY_CATEGORY, ChartConstants.DUMMY_RATE);
+        }
+
+        return categoryCharts.stream()
+                .collect(Collectors.toMap(
+                        CategoryChart::getName,
+                        categoryChart -> Math.max(categoryChart.getRate(), ChartConstants.AGGREGATED_DATE_MIN_RATE),
+                        (e1, e2) -> e1,
+                        LinkedHashMap::new
+                ));
+    }
+
+    public Map<String, String> getCategoryColorsFromCategoryChart(List<CategoryChart> categoryCharts) {
+        if (Objects.isEmpty(categoryCharts)) {
+            return Map.of(ChartConstants.DUMMY_CATEGORY, ChartConstants.DUMMY_COLOR);
+        }
+
+        return categoryCharts.stream()
+                .collect(Collectors.toMap(
+                        CategoryChart::getName,
+                        CategoryChart::getColor,
+                        (e1, e2) -> e1,
+                        LinkedHashMap::new
+                ));
+    }
+
+    public String calculateDifferenceAmount(Map<DateInfo, List<AccountBook>> accountBookGroupByDateInfo) {
+        List<DateInfo> dateInfos = new ArrayList<>(accountBookGroupByDateInfo.keySet());
+
+        Long current = AccountBookCalculator.sum(accountBookGroupByDateInfo.get(dateInfos.getLast()));
+        Long previous = AccountBookCalculator.sum(accountBookGroupByDateInfo.get(dateInfos.get(dateInfos.size() - 2)));
+
+        return AmountFormatter.compareAmount(current, previous);
+    }
+
+    public long calculateAverage(Map<DateInfo, List<AccountBook>> accountBookGroupByDateInfo) {
+        return AccountBookCalculator.average(accountBookGroupByDateInfo.values()).longValue();
+    }
 }
