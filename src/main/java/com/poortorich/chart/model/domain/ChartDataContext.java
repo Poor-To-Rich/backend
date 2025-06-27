96ecdfb41b144b14783607a822c96301b5964202
belight2 <chlwnsgur090@gmail.com>
2025-06-27 16:43:33 +0900

    feat: 차트 관련 모델 추가

--- src/main/java/com/poortorich/chart/model/domain/ChartDataContext.java
+++ src/main/java/com/poortorich/chart/model/domain/ChartDataContext.java
@@ -1,4 +1,16 @@
 package com.poortorich.chart.model.domain;
 
+import com.poortorich.category.entity.Category;
+import com.poortorich.global.date.domain.DateInfo;
+import com.poortorich.user.entity.User;
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
 public class ChartDataContext {
+
+    private User user;
+    private Category category;
+    private DateInfo dateInfo;
 }
--- src/main/java/com/poortorich/chart/model/domain/PaginationResult.java
+++ src/main/java/com/poortorich/chart/model/domain/PaginationResult.java
@@ -1,4 +1,12 @@
 package com.poortorich.chart.model.domain;
 
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
 public class PaginationResult {
+
+    private Boolean hasNext;
+    private String nextCursor;
 }
