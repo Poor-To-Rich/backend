a2f45e5982ff6d3dea67e01c9507aae742597ba0
belight2 <chlwnsgur090@gmail.com>
2025-06-27 13:11:36 +0900

    feat: 더미값 추가

--- src/main/java/com/poortorich/chart/constants/ChartConstants.java
+++ src/main/java/com/poortorich/chart/constants/ChartConstants.java
@@ -1,12 +1,11 @@
 package com.poortorich.chart.constants;
 
-import java.math.BigDecimal;
-
 public class ChartConstants {
 
     public static final String DUMMY_CATEGORY = "dummy";
     public static final String DUMMY_COLOR = "#e6e6e6";
-    public static final BigDecimal DUMMY_RATE = BigDecimal.valueOf(100L);
+    public static final Double DUMMY_RATE = 100.;
+    public static final Double AGGREGATED_DATE_MIN_RATE = 1.5;
 
     private ChartConstants() {
     }
