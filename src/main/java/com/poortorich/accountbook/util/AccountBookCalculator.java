d1b7accd72360963313b6fb2c39b260e74c3435f
belight2 <chlwnsgur090@gmail.com>
2025-06-28 14:23:39 +0900

    feat: 백분율 계산 BigDecimal로 수정

--- src/main/java/com/poortorich/accountbook/util/AccountBookCalculator.java
+++ src/main/java/com/poortorich/accountbook/util/AccountBookCalculator.java
@@ -3,6 +3,8 @@ package com.poortorich.accountbook.util;
 import com.poortorich.accountbook.entity.AccountBook;
 import com.poortorich.category.entity.Category;
 import io.jsonwebtoken.lang.Objects;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.LinkedHashMap;
@@ -12,7 +14,7 @@ import java.util.stream.Collectors;
 
 public class AccountBookCalculator {
 
-    private static final double PERCENTAGE_OFFSET = 100.0;
+    private static final BigDecimal PERCENTAGE_OFFSET = BigDecimal.valueOf(100.0);
 
     private AccountBookCalculator() {
     }
@@ -38,31 +40,32 @@ public class AccountBookCalculator {
                 .orElse(0.0);
     }
 
-    public static Double average(Collection<List<AccountBook>> accountBooks) {
+    public static BigDecimal average(Collection<List<AccountBook>> accountBooks) {
         if (Objects.isEmpty(accountBooks)) {
-            return 0.0;
+            return BigDecimal.valueOf(0.0);
         }
 
-        return accountBooks.stream()
-                .mapToDouble(AccountBookCalculator::sum)
-                .average()
-                .orElse(0.0);
+        return BigDecimal.valueOf(accountBooks.stream()
+                        .mapToDouble(AccountBookCalculator::sum)
+                        .average()
+                        .orElse(0.0))
+                .setScale(2, RoundingMode.HALF_UP);
     }
 
-    public static Map<Category, Double> percentages(
+    public static Map<Category, BigDecimal> percentages(
             Map<Category, List<AccountBook>> accountBookGroupByCategory
     ) {
         if (Objects.isEmpty(accountBookGroupByCategory)) {
             return Collections.emptyMap();
         }
-        Double total = accountBookGroupByCategory.values().stream()
+        BigDecimal total = BigDecimal.valueOf(accountBookGroupByCategory.values().stream()
                 .mapToDouble(AccountBookCalculator::sum)
-                .sum();
+                .sum());
 
-        Map<Category, Double> percentage = accountBookGroupByCategory.entrySet().stream()
+        Map<Category, BigDecimal> percentage = accountBookGroupByCategory.entrySet().stream()
                 .collect(Collectors.toMap(
                         Map.Entry::getKey,
-                        entry -> getRate(total, sum(entry.getValue()).doubleValue()),
+                        entry -> getRate(total, BigDecimal.valueOf(sum(entry.getValue()).doubleValue())),
                         (e1, e2) -> e1,
                         LinkedHashMap::new
                 ));
@@ -70,15 +73,11 @@ public class AccountBookCalculator {
         return adjustPercentages(percentage);
     }
 
-    private static Map<Category, Double> adjustPercentages(Map<Category, Double> rateByCategory) {
-        double totalRate = rateByCategory.values().stream()
-                .mapToDouble(Double::doubleValue)
-                .sum();
+    private static Map<Category, BigDecimal> adjustPercentages(Map<Category, BigDecimal> rateByCategory) {
+        BigDecimal totalRate = rateByCategory.values().stream()
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
 
-        double diff = PERCENTAGE_OFFSET - totalRate;
-        if (Math.abs(diff) < 1e-10) {
-            return rateByCategory;
-        }
+        BigDecimal diff = PERCENTAGE_OFFSET.subtract(totalRate);
 
         Category maxKey = rateByCategory.entrySet().stream()
                 .max(Map.Entry.comparingByValue())
@@ -86,14 +85,16 @@ public class AccountBookCalculator {
                 .orElse(null);
 
         if (Objects.isEmpty(maxKey)) {
-            rateByCategory.put(maxKey, rateByCategory.get(maxKey) + diff);
+            rateByCategory.put(maxKey, rateByCategory.get(maxKey).add(diff));
         }
 
         return rateByCategory;
     }
 
-    private static Double getRate(Double total, Double current) {
-        return current / total * PERCENTAGE_OFFSET;
+    private static BigDecimal getRate(BigDecimal total, BigDecimal current) {
+        return current.divide(total, 10, RoundingMode.HALF_UP)
+                .multiply(PERCENTAGE_OFFSET)
+                .setScale(1, RoundingMode.HALF_UP);
     }
 
 }
