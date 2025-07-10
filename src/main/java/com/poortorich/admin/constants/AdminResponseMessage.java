7ac320ed2c5df6bb011bc3bbd59fdeaa8e2f46b1
belight2 <chlwnsgur090@gmail.com>
2025-07-10 21:13:30 +0900

    feat: 관리자 응답 추가

--- src/main/java/com/poortorich/admin/constants/AdminResponseMessage.java
+++ src/main/java/com/poortorich/admin/constants/AdminResponseMessage.java
@@ -1,4 +1,7 @@
 package com.poortorich.admin.constants;
 
 public class AdminResponseMessage {
+
+    public static final String ROLE_UPDATE_SUCCESS = "사용자 역할 변경 성공";
+    public static final String ROLE_INVALID = "유효하지 않은 역할입니다.";
 }
