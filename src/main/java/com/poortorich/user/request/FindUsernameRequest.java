542bd8f048c6698278052c2ba687ef27baf41ed5
belight2 <chlwnsgur090@gmail.com>
2025-07-12 01:59:37 +0900

    feat: 아이디, 비밀번호 찾기 요청 DTO 추가

--- src/main/java/com/poortorich/user/request/FindUsernameRequest.java
+++ src/main/java/com/poortorich/user/request/FindUsernameRequest.java
@@ -1,4 +1,16 @@
 package com.poortorich.user.request;
 
+import com.poortorich.email.constants.EmailResponseMessage;
+import com.poortorich.email.validator.annotations.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class FindUsernameRequest {
+
+    @Email
+    @NotBlank(message = EmailResponseMessage.EMAIL_INVALID)
+    private String email;
 }
