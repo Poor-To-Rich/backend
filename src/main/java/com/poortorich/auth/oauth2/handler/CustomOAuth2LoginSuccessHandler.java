3fe6038b1431af733a845c9865bd4b1985d98300
belight2 <chlwnsgur090@gmail.com>
2025-07-14 21:12:32 +0900

    feat: OAuth2 인가 성공 시 작동 핸들러

--- src/main/java/com/poortorich/auth/oauth2/handler/CustomOAuth2LoginSuccessHandler.java
+++ src/main/java/com/poortorich/auth/oauth2/handler/CustomOAuth2LoginSuccessHandler.java
@@ -1,4 +1,38 @@
 package com.poortorich.auth.oauth2.handler;
 
-public class CustomOAuth2LoginSuccessHandler {
+import com.poortorich.auth.jwt.util.JwtTokenGenerator;
+import com.poortorich.auth.jwt.util.JwtTokenManager;
+import com.poortorich.auth.oauth2.domain.model.CustomOAuth2UserDetails;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class CustomOAuth2LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+
+    private final JwtTokenGenerator tokenGenerator;
+    private final JwtTokenManager tokenManager;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+                                        Authentication authentication) throws IOException, ServletException {
+        CustomOAuth2UserDetails userDetails = (CustomOAuth2UserDetails) authentication.getPrincipal();
+        String username = userDetails.getUsername();
+        String role = userDetails.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .findFirst()
+                .orElse(null);
+        String accessToken = tokenGenerator.generateAccessToken(userDetails.getUser());
+        String refreshToken = tokenGenerator.generateRefreshToken(userDetails.getUser());
+        tokenManager.setRefreshTokens(response, refreshToken);
+
+        response.sendRedirect("https://localhost:5173/");
+    }
 }
