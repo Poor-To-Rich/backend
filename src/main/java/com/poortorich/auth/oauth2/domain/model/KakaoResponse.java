7502d886fb189f22cf668806b3ee2ee1e9d89ba2
belight2 <chlwnsgur090@gmail.com>
2025-07-14 21:11:37 +0900

    feat: OAuth 인가 회원 정보 DTO 추가

--- src/main/java/com/poortorich/auth/oauth2/domain/model/KakaoResponse.java
+++ src/main/java/com/poortorich/auth/oauth2/domain/model/KakaoResponse.java
@@ -1,4 +1,30 @@
 package com.poortorich.auth.oauth2.domain.model;
 
-public class KakaoResponse {
+import java.util.Map;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class KakaoResponse implements OAuth2Response {
+
+    private Map<String, Object> attributes;
+
+    @Override
+    public String getProvider() {
+        return "kakao";
+    }
+
+    @Override
+    public String getProviderId() {
+        return attributes.get("id").toString();
+    }
+
+    @Override
+    public String getEmail() {
+        return (String) ((Map) attributes.get("kakao_account")).get("email");
+    }
+
+    @Override
+    public String getName() {
+        return (String) ((Map) attributes.get("properties")).get("nickname");
+    }
 }
