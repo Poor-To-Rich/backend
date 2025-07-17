48781ea798feaafe53814a1cef2312fe10d560a8
belight2 <chlwnsgur090@gmail.com>
2025-07-17 23:07:39 +0900

    feat: 웹소켓 테스트 ORIGINS 추가

--- src/main/java/com/poortorich/security/constants/SecurityConstants.java
+++ src/main/java/com/poortorich/security/constants/SecurityConstants.java
@@ -7,7 +7,10 @@ public class SecurityConstants {
     public static final List<String> ALLOWED_ORIGINS = List.of("http://localhost:8080",
             "https://localhost:5173",
             "https://poortorich.site",
-            "https://www.poortorich.site");
+            "https://www.poortorich.site"
+            // STOMP 연결 테스트 사이트
+            //"https://jiangxy.github.io"
+    );
     public static final List<String> ALLOWED_METHOD = List.of("GET", "POST", "PUT", "DELETE");
     public static final List<String> ALLOWED_HEADERS = List.of("authorization", "content-type", "x-auth-token");
     public static final String[] PERMIT_ALL_ENDPOINTS = {
---
+++ src/main/java/com/poortorich/websocket/interceptor/StompInterceptor.java
@@ -0,0 +1,4 @@
+package com.poortorich.websocket.interceptor;
+
+public class StompInterceptor {
+}
