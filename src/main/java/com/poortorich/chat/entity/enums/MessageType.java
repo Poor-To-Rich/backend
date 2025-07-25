ab7d18098c4110bfbac9e4a4bf2ca7b4a91d3a89
sseen2 <kse10023@naver.com>
2025-07-26 01:54:55 +0900

    feat: 채팅 메세지 entity 추가

---
+++ src/main/java/com/poortorich/chat/entity/enums/MessageType.java
@@ -0,0 +1,14 @@
+package com.poortorich.chat.entity.enums;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum MessageType {
+
+    TEXT,
+    PHOTO,
+    RANKING,
+    SYSTEM
+}
