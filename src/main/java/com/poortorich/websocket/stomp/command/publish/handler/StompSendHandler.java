3ac5d521a97bea5769d7a71146fc76eca926006a
belight2 <chlwnsgur090@gmail.com>
2025-07-26 14:31:11 +0900

    feat: STOMP 발행 핸들러 추가
    엔드포인트만 검증
    역직렬화가 완료된 컨트롤러에서 검증 로직을 수행할 예정

--- src/main/java/com/poortorich/websocket/stomp/command/publish/handler/StompSendHandler.java
+++ src/main/java/com/poortorich/websocket/stomp/command/publish/handler/StompSendHandler.java
@@ -1,5 +1,7 @@
-package com.poortorich.websocket.stomp.handler;
+package com.poortorich.websocket.stomp.command.publish.handler;
 
+import com.poortorich.websocket.stomp.command.publish.validator.StompPublishValidator;
+import com.poortorich.websocket.stomp.validator.StompValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
@@ -8,7 +10,11 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class StompSendHandler {
 
+    private final StompValidator stompValidator;
+    private final StompPublishValidator stompPublishValidator;
+
     public void handle(StompHeaderAccessor accessor) {
-        
+        stompValidator.validateDestination(accessor);
+        stompPublishValidator.validate(accessor);
     }
 }
--- src/main/java/com/poortorich/websocket/stomp/command/publish/validator/StompPublishValidator.java
+++ src/main/java/com/poortorich/websocket/stomp/command/publish/validator/StompPublishValidator.java
@@ -1,4 +1,23 @@
 package com.poortorich.websocket.stomp.command.publish.validator;
 
+import com.poortorich.global.exceptions.BadRequestException;
+import com.poortorich.websocket.stomp.command.publish.endpoint.PublishEndpoint;
+import com.poortorich.websocket.stomp.response.StompResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
 public class StompPublishValidator {
+
+    public void validate(StompHeaderAccessor accessor) {
+        String destination = accessor.getDestination();
+        boolean isMatching = PublishEndpoint.PUBLISH_PREFIXES.stream()
+                .anyMatch(prefix -> destination.startsWith(prefix));
+
+        if (!isMatching) {
+            throw new BadRequestException(StompResponse.DESTINATION_INVALID);
+        }
+    }
 }
