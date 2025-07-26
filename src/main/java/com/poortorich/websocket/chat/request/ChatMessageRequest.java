bcaaa0b9b75c0730a6e9ada82ccf36556de5e68f
belight2 <chlwnsgur090@gmail.com>
2025-07-26 14:32:23 +0900

    feat: 테스트용 발행 로직 추가

--- src/main/java/com/poortorich/websocket/chat/request/ChatMessageRequest.java
+++ src/main/java/com/poortorich/websocket/chat/request/ChatMessageRequest.java
@@ -1,4 +1,13 @@
 package com.poortorich.websocket.chat.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class ChatMessageRequest {
+
+    Long chatroomId;
+    String messageType;
+    String content;
 }
