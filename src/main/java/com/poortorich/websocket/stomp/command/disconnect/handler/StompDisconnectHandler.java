7d04b95d6ebe22bbfbdda5a637a1ad8aef3adea2
belight2 <chlwnsgur090@gmail.com>
2025-07-26 14:31:58 +0900

    feat: StompInterceptor 로직 수정

--- src/main/java/com/poortorich/websocket/stomp/command/disconnect/handler/StompDisconnectHandler.java
+++ src/main/java/com/poortorich/websocket/stomp/command/disconnect/handler/StompDisconnectHandler.java
@@ -1,4 +1,4 @@
-package com.poortorich.websocket.stomp.handler;
+package com.poortorich.websocket.stomp.command.disconnect.handler;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
