cef1ebc2453d89b2c92f911016ce5169c3b9c4d2
belight2 <chlwnsgur090@gmail.com>
2025-07-26 14:25:42 +0900

    feat: Stomp 헤더 추출기 추가

--- src/main/java/com/poortorich/websocket/stomp/util/StompHeaderExtractor.java
+++ src/main/java/com/poortorich/websocket/stomp/util/StompHeaderExtractor.java
@@ -1,4 +1,18 @@
 package com.poortorich.websocket.stomp.util;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
 public class StompHeaderExtractor {
+
+    public String extractAccessToken(StompHeaderAccessor accessor) {
+        return extractByHeaderName(accessor, "Authorization");
+    }
+
+    private String extractByHeaderName(StompHeaderAccessor accessor, String headerName) {
+        return accessor.getFirstNativeHeader(headerName);
+    }
 }
