feba6520a9145022b2f288edecf4b6e4efdf9651
sseen2 <kse10023@naver.com>
2025-07-28 00:43:15 +0900

    refactor: 불필요한 import문 삭제

--- src/main/java/com/poortorich/chat/service/ChatParticipantService.java
+++ src/main/java/com/poortorich/chat/service/ChatParticipantService.java
@@ -3,8 +3,6 @@ package com.poortorich.chat.service;
 import com.poortorich.chat.entity.ChatParticipant;
 import com.poortorich.chat.entity.Chatroom;
 import com.poortorich.chat.entity.enums.ChatroomRole;
-import com.poortorich.chat.entity.enums.NoticeStatus;
-import com.poortorich.chat.entity.enums.RankingStatus;
 import com.poortorich.chat.repository.ChatParticipantRepository;
 import com.poortorich.chat.util.ChatBuilder;
 import com.poortorich.user.entity.User;
