3b1f77323f84edb2ed591e5bed95f87994b153b4
sseen2 <kse10023@naver.com>
2025-07-29 03:20:54 +0900

    refactor: EntityScan 어노테이션 삭제

--- src/main/java/com/poortorich/PoorToRichApplication.java
+++ src/main/java/com/poortorich/PoorToRichApplication.java
@@ -5,16 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EntityScan(basePackages = {
-        "com.poortorich.category.entity",
-        "com.poortorich.user.entity",
-        "com.poortorich.expense.entity",
-        "com.poortorich.income.entity",
-        "com.poortorich.iteration.entity",
-        "com.poortorich.chat.entity",
-        "com.poortorich.like.entity",
-        "com.poortorich.tag.entity"
-})
 public class PoorToRichApplication {
 
     public static void main(String[] args) {
