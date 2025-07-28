ff3d1e2ed3dd5115e6642cb1c40eb314d48d89b3
sseen2 <kse10023@naver.com>
2025-07-29 03:07:25 +0900

    refactor: 하나의 request로 입력받도록 수정

--- src/main/java/com/poortorich/chat/request/ChatroomCreateRequest.java
+++ src/main/java/com/poortorich/chat/request/ChatroomCreateRequest.java
@@ -7,13 +7,18 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
-@Getter
+@Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class ChatroomCreateRequest {
+
+    private MultipartFile chatroomImage;
 
     @NotNull(message = ChatResponseMessage.CHATROOM_TITLE_REQUIRED)
     @Size(max = ChatValidationConstraints.CHATROOM_TITLE_MAX,
