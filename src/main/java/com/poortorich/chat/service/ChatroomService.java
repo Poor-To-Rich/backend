22ca5f4b829ae9a7b438ef9afd682f57c2558929
belight2 <chlwnsgur090@gmail.com>
2025-07-29 13:53:45 +0900

    feat: 채팅방 조회 추가

--- src/main/java/com/poortorich/chat/service/ChatroomService.java
+++ src/main/java/com/poortorich/chat/service/ChatroomService.java
@@ -3,7 +3,9 @@ package com.poortorich.chat.service;
 import com.poortorich.chat.entity.Chatroom;
 import com.poortorich.chat.repository.ChatroomRepository;
 import com.poortorich.chat.request.ChatroomCreateRequest;
+import com.poortorich.chat.response.enums.ChatResponse;
 import com.poortorich.chat.util.ChatBuilder;
+import com.poortorich.global.exceptions.NotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -16,5 +18,10 @@ public class ChatroomService {
     public Chatroom createChatroom(String imageUrl, ChatroomCreateRequest request) {
         Chatroom chatroom = ChatBuilder.buildChatroom(imageUrl, request);
         return chatroomRepository.save(chatroom);
+    }
+
+    public Chatroom findByChatroomId(Long chatroomId) {
+        return chatroomRepository.findById(chatroomId)
+                .orElseThrow(() -> new NotFoundException(ChatResponse.CHATROOM_NOT_FOUND));
     }
 }
