78caf13ee9e39cc023b1f54383430734c0d83d38
belight2 <chlwnsgur090@gmail.com>
2025-08-03 16:48:37 +0900

    feat: payloadContext 적용

--- src/main/java/com/poortorich/chat/realtime/model/PayloadContext.java
+++ src/main/java/com/poortorich/chat/realtime/model/PayloadContext.java
@@ -1,4 +1,10 @@
 package com.poortorich.chat.realtime.model;
 
-public record PayloadContext() {
+import com.poortorich.chat.entity.ChatParticipant;
+import com.poortorich.chat.entity.Chatroom;
+import com.poortorich.user.entity.User;
+import lombok.Builder;
+
+@Builder
+public record PayloadContext(User user, Chatroom chatroom, ChatParticipant chatParticipant) {
 }
