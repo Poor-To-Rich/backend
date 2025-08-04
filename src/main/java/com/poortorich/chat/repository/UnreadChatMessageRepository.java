b458f86c1fe851d837cedc7c6adadd2cf10db642
belight2 <chlwnsgur090@gmail.com>
2025-08-04 10:22:55 +0900

    feat: 안읽은 사람 create

--- src/main/java/com/poortorich/chat/repository/UnreadChatMessageRepository.java
+++ src/main/java/com/poortorich/chat/repository/UnreadChatMessageRepository.java
@@ -1,4 +1,9 @@
 package com.poortorich.chat.repository;
 
-public interface UnreadChatMessageRepository {
+import com.poortorich.chat.entity.UnreadChatMessage;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UnreadChatMessageRepository extends JpaRepository<UnreadChatMessage, Long> {
 }
--- src/main/java/com/poortorich/chat/service/UnreadChatMessageService.java
+++ src/main/java/com/poortorich/chat/service/UnreadChatMessageService.java
@@ -1,4 +1,32 @@
 package com.poortorich.chat.service;
 
+import com.poortorich.chat.entity.ChatMessage;
+import com.poortorich.chat.entity.ChatParticipant;
+import com.poortorich.chat.entity.UnreadChatMessage;
+import com.poortorich.chat.repository.UnreadChatMessageRepository;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class UnreadChatMessageService {
+
+    private final UnreadChatMessageRepository unreadChatMessageRepository;
+
+    public List<Long> saveUnreadMember(ChatMessage chatMessage, List<ChatParticipant> chatMembers) {
+        return chatMembers.stream()
+                .map(chatParticipant -> {
+                    UnreadChatMessage unreadChatMessage = UnreadChatMessage.builder()
+                            .user(chatParticipant.getUser())
+                            .chatroom(chatParticipant.getChatroom())
+                            .chatMessage(chatMessage)
+                            .build();
+
+                    unreadChatMessageRepository.save(unreadChatMessage);
+
+                    return chatParticipant.getUser().getId();
+                })
+                .toList();
+    }
 }
