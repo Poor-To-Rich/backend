f3b2905f72ed4df580a3d4fc65560faa6bc29256
sseen2 <kse10023@naver.com>
2025-08-05 00:28:46 +0900

    refactor: response 기본생성자 어노테이션 추가

--- src/main/java/com/poortorich/chat/response/ChatroomCoverInfoResponse.java
+++ src/main/java/com/poortorich/chat/response/ChatroomCoverInfoResponse.java
@@ -3,11 +3,13 @@ package com.poortorich.chat.response;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Getter
 @Builder
+@NoArgsConstructor
 @AllArgsConstructor
 public class ChatroomCoverInfoResponse {
 
