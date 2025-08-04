a43dae68163b9829d3c81527081923e634f794c5
belight2 <chlwnsgur090@gmail.com>
2025-08-04 10:28:29 +0900

    feat: 채팅 메시지 발행 로직 추가

--- src/main/java/com/poortorich/chat/realtime/controller/ChatRealtimeController.java
+++ src/main/java/com/poortorich/chat/realtime/controller/ChatRealtimeController.java
@@ -1,10 +1,16 @@
 package com.poortorich.chat.realtime.controller;
 
 import com.poortorich.chat.realtime.facade.ChatRealTimeFacade;
+import com.poortorich.chat.realtime.payload.ResponsePayload;
+import com.poortorich.chat.realtime.payload.request.ChatMessageRequestPayload;
+import com.poortorich.websocket.stomp.command.subscribe.endpoint.SubscribeEndpoint;
 import com.poortorich.websocket.stomp.util.StompSessionManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Controller;
 
 @Slf4j
@@ -16,4 +22,17 @@ public class ChatRealtimeController {
 
     private final SimpMessagingTemplate messagingTemplate;
     private final StompSessionManager sessionManager;
+
+    @MessageMapping("/chat/messages")
+    public void sendChatMessage(
+            StompHeaderAccessor accessor,
+            @Payload ChatMessageRequestPayload chatMessagePayload
+    ) {
+        String username = sessionManager.getUsername(accessor);
+        ResponsePayload payload = chatRealTimeFacade.createUserChatMessage(username, chatMessagePayload);
+
+        messagingTemplate.convertAndSend(
+                SubscribeEndpoint.CHATROOM_SUBSCRIBE_PREFIX + chatMessagePayload.getChatroomId(),
+                payload);
+    }
 }
--- src/main/java/com/poortorich/chat/realtime/facade/ChatRealTimeFacade.java
+++ src/main/java/com/poortorich/chat/realtime/facade/ChatRealTimeFacade.java
@@ -1,14 +1,20 @@
 package com.poortorich.chat.realtime.facade;
 
+import com.poortorich.chat.entity.ChatParticipant;
 import com.poortorich.chat.entity.Chatroom;
 import com.poortorich.chat.entity.enums.ChatMessageType;
 import com.poortorich.chat.realtime.payload.ResponsePayload;
+import com.poortorich.chat.realtime.payload.UserChatMessagePayload;
 import com.poortorich.chat.realtime.payload.UserEnterResponsePayload;
 import com.poortorich.chat.realtime.payload.UserLeaveResponsePayload;
+import com.poortorich.chat.realtime.payload.request.ChatMessageRequestPayload;
 import com.poortorich.chat.service.ChatMessageService;
+import com.poortorich.chat.service.ChatParticipantService;
 import com.poortorich.chat.service.ChatroomService;
+import com.poortorich.chat.service.UnreadChatMessageService;
 import com.poortorich.user.entity.User;
 import com.poortorich.user.service.UserService;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -19,6 +25,8 @@ public class ChatRealTimeFacade {
     private final UserService userService;
     private final ChatroomService chatroomService;
     private final ChatMessageService chatMessageService;
+    private final ChatParticipantService chatParticipantService;
+    private final UnreadChatMessageService unreadChatMessageService;
 
     public ResponsePayload createUserEnterSystemMessage(String username, Long chatroomId) {
         User user = userService.findUserByUsername(username);
@@ -41,6 +49,24 @@ public class ChatRealTimeFacade {
         return ResponsePayload.builder()
                 .type(ChatMessageType.SYSTEM_MESSAGE)
                 .payload(payload)
+                .build();
+    }
+
+    public ResponsePayload createUserChatMessage(String username, ChatMessageRequestPayload chatMessagePayload) {
+        User user = userService.findUserByUsername(username);
+        Chatroom chatroom = chatroomService.findById(chatMessagePayload.getChatroomId());
+        ChatParticipant chatParticipant = chatParticipantService.findByUserAndChatroom(user, chatroom);
+
+        List<ChatParticipant> chatMembers = chatParticipantService.findAllByChatroom(chatroom).stream()
+                .filter(participant -> !participant.getUser().getId().equals(chatParticipant.getUser().getId()))
+                .toList();
+
+        UserChatMessagePayload chatMessage = chatMessageService
+                .saveUserChatMessage(chatParticipant, chatMembers, chatMessagePayload);
+
+        return ResponsePayload.builder()
+                .type(ChatMessageType.CHAT_MESSAGE)
+                .payload(chatMessage)
                 .build();
     }
 }
