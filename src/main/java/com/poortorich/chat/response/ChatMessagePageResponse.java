a787e3c8a785afa27de065e14aad16530587d0ae
belight2 <chlwnsgur090@gmail.com>
2025-08-04 21:05:21 +0900

    feat: 이전 채팅 조회 응답 DTO 추가

--- src/main/java/com/poortorich/chat/response/ChatMessagePageResponse.java
+++ src/main/java/com/poortorich/chat/response/ChatMessagePageResponse.java
@@ -1,4 +1,17 @@
 package com.poortorich.chat.response;
 
+import com.poortorich.chat.model.ChatMessageResponse;
+import java.util.List;
+import java.util.Map;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class ChatMessagePageResponse {
+
+    private Long nextCursor;
+    private Boolean hasNext;
+    private List<ChatMessageResponse> messages;
+    private Map<Long, ChatParticipantProfile> users;
 }
