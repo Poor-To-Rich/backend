bd8b71d372b6b54648adcc752733fbda599a0dac
sseen2 <kse10023@naver.com>
2025-08-05 23:58:52 +0900

    feat: 좋아요 상태 및 개수 조회 로직 추가

---
+++ src/main/java/com/poortorich/like/service/LikeService.java
@@ -0,0 +1,25 @@
+package com.poortorich.like.service;
+
+import com.poortorich.chat.entity.Chatroom;
+import com.poortorich.like.entity.Like;
+import com.poortorich.like.repository.LikeRepository;
+import com.poortorich.user.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LikeService {
+
+    private final LikeRepository likeRepository;
+
+    public Boolean getLikeStatus(User user, Chatroom chatroom) {
+        return likeRepository.findByUserAndChatroom(user, chatroom)
+                .map(Like::getLikeStatus)
+                .orElse(false);
+    }
+
+    public Long getLikeCount(Chatroom chatroom) {
+        return likeRepository.countByChatroom(chatroom);
+    }
+}
