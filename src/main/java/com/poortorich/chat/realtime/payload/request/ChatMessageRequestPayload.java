092eb2b9f4a28a2bf1f0652489cd5a5ee019264b
belight2 <chlwnsgur090@gmail.com>
2025-08-05 13:42:00 +0900

    feat: 채팅 메시지 발행 페이로드 검증 로직 추가

--- src/main/java/com/poortorich/chat/realtime/payload/request/ChatMessageRequestPayload.java
+++ src/main/java/com/poortorich/chat/realtime/payload/request/ChatMessageRequestPayload.java
@@ -1,6 +1,8 @@
 package com.poortorich.chat.realtime.payload.request;
 
+import com.poortorich.chat.constants.ChatResponseMessage;
 import com.poortorich.chat.entity.enums.MessageType;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -8,7 +10,11 @@ import lombok.Getter;
 @AllArgsConstructor
 public class ChatMessageRequestPayload {
 
+    @NotNull(message = ChatResponseMessage.CHATROOM_ID_REQUIRED)
     private Long chatroomId;
+
+    @NotNull(message = ChatResponseMessage.MESSAGE_TPYE_REQUIRED)
     private MessageType messageType;
+
     private String content;
 }
