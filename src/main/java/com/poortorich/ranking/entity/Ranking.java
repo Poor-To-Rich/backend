83e763202f2c0a37dbf008f02074d372a77684e7
sseen2 <kse10023@naver.com>
2025-08-06 12:06:56 +0900

    feat: 랭킹 entity 추가

---
+++ src/main/java/com/poortorich/ranking/entity/Ranking.java
@@ -0,0 +1,55 @@
+package com.poortorich.ranking.entity;
+
+import com.poortorich.chat.entity.Chatroom;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicUpdate;
+
+@Entity
+@Getter
+@Builder
+@DynamicUpdate
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "ranking")
+public class Ranking {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "saver_first")
+    private Long saverFirst;
+
+    @Column(name = "saver_seconed")
+    private Long saverSecond;
+
+    @Column(name = "saver_third")
+    private Long saverThird;
+
+    @Column(name = "flexer_first")
+    private Long flexerFirst;
+
+    @Column(name = "flexer_second")
+    private Long flexerSecond;
+
+    @Column(name = "flexer_third")
+    private Long flexerThird;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "chatroom_id", nullable = false)
+    private Chatroom chatroom;
+}
