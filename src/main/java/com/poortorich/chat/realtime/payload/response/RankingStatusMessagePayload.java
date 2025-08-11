95afe55fea3c1803c7f5ddf603b61d52dc0c2981
belight2 <chlwnsgur090@gmail.com>
2025-08-11 15:06:59 +0900

    feat: 랭킹 활성화 메시지 오류 수정
    이전 메시지 조회에서 오류가 발생 이를 수정

--- src/main/java/com/poortorich/chat/realtime/payload/response/RankingStatusMessagePayload.java
+++ src/main/java/com/poortorich/chat/realtime/payload/response/RankingStatusMessagePayload.java
@@ -1,6 +1,7 @@
 package com.poortorich.chat.realtime.payload.response;
 
 import com.poortorich.chat.entity.enums.ChatMessageType;
+import com.poortorich.chat.model.ChatMessageResponse;
 import com.poortorich.chat.realtime.payload.interfaces.ResponsePayload;
 import java.time.LocalDateTime;
 import lombok.Builder;
@@ -8,7 +9,7 @@ import lombok.Data;
 
 @Data
 @Builder
-public class RankingStatusMessagePayload implements ResponsePayload {
+public class RankingStatusMessagePayload extends ChatMessageResponse implements ResponsePayload {
 
     private Long messageId;
     private Long chatroomId;
