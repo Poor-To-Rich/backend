3064a8fee53f42e31ae5a3d7edeb0e335cb12814
sseen2 <kse10023@naver.com>
2025-08-13 11:53:26 +0900

    feat: 응답 제약사항 추가

--- src/main/java/com/poortorich/report/constants/ReportResponseMessage.java
+++ src/main/java/com/poortorich/report/constants/ReportResponseMessage.java
@@ -6,6 +6,8 @@ public class ReportResponseMessage {
 
     public static final String REPORT_REASON_REQUIRED = "신고 사유는 필수입니다.";
     public static final String REPORT_DUPLICATE = "현재 채팅방에서 이미 이 참여자를 신고했습니다.";
+    public static final String CUSTOM_REASON_TOO_BIG
+            = "기타 사유는 " + ReportValidationConstraints.CUSTOM_REASON_MAX + "자 이하여야 합니다.";
 
     private ReportResponseMessage() {
     }
--- src/main/java/com/poortorich/report/request/ReceiptReportRequest.java
+++ src/main/java/com/poortorich/report/request/ReceiptReportRequest.java
@@ -1,7 +1,9 @@
 package com.poortorich.report.request;
 
 import com.poortorich.report.constants.ReportResponseMessage;
+import com.poortorich.report.constants.ReportValidationConstraints;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -11,5 +13,8 @@ public class ReceiptReportRequest {
 
     @NotNull(message = ReportResponseMessage.REPORT_REASON_REQUIRED)
     private String reportType;
+
+    @Size(max = ReportValidationConstraints.CUSTOM_REASON_MAX,
+            message = ReportResponseMessage.CUSTOM_REASON_TOO_BIG)
     private String customReason;
 }
