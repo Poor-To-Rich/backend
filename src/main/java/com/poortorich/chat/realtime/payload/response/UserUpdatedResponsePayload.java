60fa558bad7220d7df9d4d700d2f6a5a1c010090
belight2 <chlwnsgur090@gmail.com>
2025-08-13 15:36:34 +0900

    feat: nickname 필드 추가

--- src/main/java/com/poortorich/chat/realtime/payload/response/UserUpdatedResponsePayload.java
+++ src/main/java/com/poortorich/chat/realtime/payload/response/UserUpdatedResponsePayload.java
@@ -1,16 +1,27 @@
 package com.poortorich.chat.realtime.payload.response;
 
 import com.poortorich.chat.entity.enums.RankingStatus;
+import com.poortorich.chat.realtime.payload.interfaces.ResponsePayload;
+import com.poortorich.chat.realtime.payload.response.enums.PayloadType;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
-public class UserUpdatedResponsePayload {
+public class UserUpdatedResponsePayload implements ResponsePayload {
 
     private final Long userId;
     private final String profileImage;
     private final Boolean isDefaultProfile;
+    private final String nickname;
     private final Boolean isHost;
     private final RankingStatus rankingType;
+
+    @Override
+    public BasePayload mapToBasePayload() {
+        return BasePayload.builder()
+                .type(PayloadType.USER_UPDATED)
+                .payload(this)
+                .build();
+    }
 }
