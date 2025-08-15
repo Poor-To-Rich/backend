271b72f8f695abfd56e368c5c6d5fb656f340cf9
belight2 <chlwnsgur090@gmail.com>
2025-08-15 16:58:07 +0900

    style: 임포트 형식 변경

--- src/main/java/com/poortorich/chat/controller/ChatController.java
+++ src/main/java/com/poortorich/chat/controller/ChatController.java
@@ -23,7 +23,6 @@ import com.poortorich.global.response.DataResponse;
 import com.poortorich.websocket.stomp.command.subscribe.endpoint.SubscribeEndpoint;
 import jakarta.annotation.Nullable;
 import jakarta.validation.Valid;
-import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -41,6 +40,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/chatrooms")
