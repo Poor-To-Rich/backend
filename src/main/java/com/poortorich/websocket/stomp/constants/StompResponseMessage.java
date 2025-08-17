8f858a694ba129fd836726951fa82450981168ce
belight2 <chlwnsgur090@gmail.com>
2025-08-17 18:11:03 +0900

    feat: 구독 정보를 저장하는 서비스 로직 추가

--- src/main/java/com/poortorich/websocket/stomp/constants/StompResponseMessage.java
+++ src/main/java/com/poortorich/websocket/stomp/constants/StompResponseMessage.java
@@ -7,4 +7,6 @@ public class StompResponseMessage {
     public static final String DESTINATION_INVALID = "엔드포인트가 유효하지 않습니다.";
 
     public static final String UNSUBSCRIBE_FORBIDDEN = "구독 해제가 불가합니다.";
+    public static final String SUBSCRIBER_SAVE_FAILURE = "구독 정보를 저장할 수 없습니다. 잠시 후 다시 시도해주세요.";
+    public static final String UNSUBSCRIBER_REMOVE_FAILURE = "구독 정보를 해제할 수 없습니다. 잠시 후 다시 시도해주세요.";
 }
--- src/main/java/com/poortorich/websocket/stomp/response/StompResponse.java
+++ src/main/java/com/poortorich/websocket/stomp/response/StompResponse.java
@@ -11,7 +11,9 @@ public enum StompResponse implements Response {
     SESSION_ATTRIBUTE_INVALID(HttpStatus.BAD_REQUEST, StompResponseMessage.SESSION_ATTRIBUTE_INVALID, "STOMP"),
     DESTINATION_NOT_FOUND(HttpStatus.NOT_FOUND, StompResponseMessage.DESTINATION_NOT_FOUND, "STOMP"),
     DESTINATION_INVALID(HttpStatus.BAD_REQUEST, StompResponseMessage.DESTINATION_INVALID, "STOMP"),
-    UNSUBSCRIBE_FORBIDDEN(HttpStatus.FORBIDDEN, StompResponseMessage.UNSUBSCRIBE_FORBIDDEN, "STOMP");
+    UNSUBSCRIBE_FORBIDDEN(HttpStatus.FORBIDDEN, StompResponseMessage.UNSUBSCRIBE_FORBIDDEN, "STOMP"),
+    SUBSCRIBER_SAVE_FAILURE(HttpStatus.INTERNAL_SERVER_ERROR, StompResponseMessage.SUBSCRIBER_SAVE_FAILURE, null),
+    UNSUBSCRIBER_REMOVE_FAILURE(HttpStatus.INTERNAL_SERVER_ERROR, StompResponseMessage.UNSUBSCRIBER_REMOVE_FAILURE, null);
 
     private final HttpStatus httpStatus;
     private final String message;
