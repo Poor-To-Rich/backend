3b6f76e61fa6740a1d876b5b452bfc9ce3e00fd0
sseen2 <kse10023@naver.com>
2025-08-20 02:24:30 +0900

    style: 공통 endpoint 삭제

--- src/main/java/com/poortorich/chat/controller/ChatParticipantController.java
+++ src/main/java/com/poortorich/chat/controller/ChatParticipantController.java
@@ -9,17 +9,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/users")
 @RequiredArgsConstructor
 public class ChatParticipantController {
 
     private final ChatFacade chatFacade;
 
-    @GetMapping("/hosted-chatrooms")
+    @GetMapping("/users/hosted-chatrooms")
     public ResponseEntity<BaseResponse> getHostedChatrooms(@AuthenticationPrincipal UserDetails userDetails) {
         return DataResponse.toResponseEntity(
                 ChatResponse.GET_HOSTED_CHATROOMS_SUCCESS,
