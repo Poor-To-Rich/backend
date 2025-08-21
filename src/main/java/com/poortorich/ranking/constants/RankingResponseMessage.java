eb744f571751f88a1ff262424478c3585c727f66
sseen2 <kse10023@naver.com>
2025-08-21 12:01:34 +0900

    style: 성공 메시지 수정

--- src/main/java/com/poortorich/ranking/constants/RankingResponseMessage.java
+++ src/main/java/com/poortorich/ranking/constants/RankingResponseMessage.java
@@ -6,6 +6,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class RankingResponseMessage {
 
-    public static final String GET_LATEST_RANKING_SUCCESS = "최신 랭킹 조회에 완료했습니다.";
+    public static final String GET_LATEST_RANKING_SUCCESS = "최신 랭킹 조회를 완료했습니다.";
     public static final String GET_LATEST_RANKING_NOT_FOUND = "랭킹이 집계되지 않았습니다.";
 }
