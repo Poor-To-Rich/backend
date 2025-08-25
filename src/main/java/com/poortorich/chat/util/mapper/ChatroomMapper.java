52de8c0ec62978c4f06d3d8a01a33b09b60f3b71
belight2 <chlwnsgur090@gmail.com>
2025-08-25 21:56:13 +0900

    feat: 응답 DTO Mapper 로직 추가

--- src/main/java/com/poortorich/chat/util/mapper/ChatroomMapper.java
+++ src/main/java/com/poortorich/chat/util/mapper/ChatroomMapper.java
@@ -1,4 +1,41 @@
 package com.poortorich.chat.util.mapper;
 
+import com.poortorich.chat.entity.ChatMessage;
+import com.poortorich.chat.entity.ChatParticipant;
+import com.poortorich.chat.entity.Chatroom;
+import com.poortorich.chat.entity.enums.ChatroomRole;
+import com.poortorich.chat.response.MyChatroom;
+import com.poortorich.chat.service.ChatMessageService;
+import com.poortorich.chat.service.ChatParticipantService;
+import com.poortorich.chat.service.UnreadChatMessageService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
 public class ChatroomMapper {
+
+    private final UnreadChatMessageService unreadChatMessageService;
+    private final ChatMessageService chatMessageService;
+    private final ChatParticipantService chatParticipantService;
+
+    public MyChatroom mapToMyChatroom(ChatParticipant participant) {
+        Chatroom chatroom = participant.getChatroom();
+
+        ChatMessage lastMessage = chatMessageService.getLastMessage(participant.getChatroom());
+        Long unreadMessageCount = unreadChatMessageService.countByUnreadChatMessage(
+                participant.getUser(),
+                participant.getChatroom());
+
+        return MyChatroom.builder()
+                .chatroomId(chatroom.getId())
+                .chatroomImage(chatroom.getImage())
+                .isHost(ChatroomRole.HOST.equals(participant.getRole()))
+                .chatroomTitle(chatroom.getTitle())
+                .currentMemberCount(chatParticipantService.countByChatroom(chatroom))
+                .lastMessage(lastMessage.getContent())
+                .lastMessageTime(lastMessage.getSentAt())
+                .unreadMessageCount(unreadMessageCount)
+                .build();
+    }
 }
