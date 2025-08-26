aeb8c4b64bdc953d43a76eda692f278401db1283
belight2 <chlwnsgur090@gmail.com>
2025-08-26 20:45:54 +0900

    feat: 브로드캐스트 로직 추가

--- src/main/java/com/poortorich/chat/realtime/event/chatroom/ChatroomUpdateEventListner.java
+++ src/main/java/com/poortorich/chat/realtime/event/chatroom/ChatroomUpdateEventListner.java
@@ -1,7 +1,10 @@
 package com.poortorich.chat.realtime.event.chatroom;
 
 import com.poortorich.chat.entity.ChatParticipant;
+import com.poortorich.chat.response.MyChatroom;
 import com.poortorich.chat.service.ChatParticipantService;
+import com.poortorich.chat.util.mapper.ChatroomMapper;
+import com.poortorich.websocket.stomp.command.subscribe.endpoint.SubscribeEndpoint;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Component;
@@ -16,13 +19,18 @@ public class ChatroomUpdateEventListner {
 
     private final SimpMessagingTemplate messagingTemplate;
     private final ChatParticipantService chatParticipantService;
+    private final ChatroomMapper chatroomMapper;
 
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void onChatroomUpdated(ChatroomUpdateEvent event) {
         List<ChatParticipant> participants = chatParticipantService.findAllByChatroom(event.getChatroom());
 
         participants.forEach(participant -> {
-            
+            MyChatroom myChatroom = chatroomMapper.mapToMyChatroom(participant);
+
+            messagingTemplate.convertAndSend(
+                    SubscribeEndpoint.CHATROOM_SUBSCRIBE_PREFIX + participant.getUser().getId(),
+                    myChatroom);
         });
     }
 }
