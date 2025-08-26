36d81fadcb9a7c05f5f10e04a3a70251aa1ec3ab
belight2 <chlwnsgur090@gmail.com>
2025-08-26 17:23:05 +0900

    feat: 참여중인 채팅방 목록 구독 엔드포인트 추가

--- src/main/java/com/poortorich/websocket/stomp/command/subscribe/endpoint/SubscribeEndpoint.java
+++ src/main/java/com/poortorich/websocket/stomp/command/subscribe/endpoint/SubscribeEndpoint.java
@@ -5,6 +5,7 @@ import java.util.List;
 public class SubscribeEndpoint {
 
     public static final String CHATROOM_SUBSCRIBE_PREFIX = "/sub/chatroom/";
+    public static final String JOINED_CHATROOM_LIST_PREFIX = "/sub/chat/summary";
 
-    public static final List<String> SUB_PREFIXES = List.of(CHATROOM_SUBSCRIBE_PREFIX);
+    public static final List<String> SUB_PREFIXES = List.of(CHATROOM_SUBSCRIBE_PREFIX, JOINED_CHATROOM_LIST_PREFIX);
 }
