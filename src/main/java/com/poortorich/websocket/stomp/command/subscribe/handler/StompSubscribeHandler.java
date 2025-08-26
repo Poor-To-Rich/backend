71db930a90f719927b426b7bf01813904cf9bbb3
belight2 <chlwnsgur090@gmail.com>
2025-08-26 17:49:29 +0900

    feat: 새로운 구독 경로 검증 로직 추가
    1. getChatroomId를 getDestinationVariable로 메소드명 수정
    2. 검증 로직을 핸들러에서 검증기로 이동
    3. 새로운 구독 경로에 대한 검증 로직 추가
    4. 핸들러에서는 prefix에 따라 올바른 검증 로직을 호출하도록 조정하는
       역할

--- src/main/java/com/poortorich/websocket/stomp/command/subscribe/handler/StompSubscribeHandler.java
+++ src/main/java/com/poortorich/websocket/stomp/command/subscribe/handler/StompSubscribeHandler.java
@@ -1,21 +1,22 @@
 package com.poortorich.websocket.stomp.command.subscribe.handler;
 
-import com.poortorich.chat.entity.Chatroom;
 import com.poortorich.chat.repository.ChatroomRepository;
-import com.poortorich.chat.response.enums.ChatResponse;
 import com.poortorich.chat.validator.ChatroomValidator;
+import com.poortorich.global.exceptions.BadRequestException;
 import com.poortorich.global.exceptions.NotFoundException;
-import com.poortorich.user.entity.User;
 import com.poortorich.user.repository.UserRepository;
-import com.poortorich.user.response.enums.UserResponse;
+import com.poortorich.websocket.stomp.command.subscribe.endpoint.SubscribeEndpoint;
 import com.poortorich.websocket.stomp.command.subscribe.util.SubscribeEndpointExtractor;
 import com.poortorich.websocket.stomp.command.subscribe.validator.SubscribeValidator;
+import com.poortorich.websocket.stomp.response.StompResponse;
 import com.poortorich.websocket.stomp.service.SubscribeService;
 import com.poortorich.websocket.stomp.util.StompSessionManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
+
+import java.util.Objects;
 
 @Slf4j
 @Component
@@ -34,19 +35,19 @@ public class StompSubscribeHandler {
     public void handle(StompHeaderAccessor accessor) {
         log.info("[SUBSCRIBE]: 구독 시작---------------------------");
         subscribeValidator.validateEndPoint(accessor);
-        Long chatroomId = endpointExtractor.getChatroomId(accessor.getDestination());
-        Chatroom chatroom = chatroomRepository.findById(chatroomId)
-                .orElseThrow(() -> new NotFoundException(ChatResponse.CHATROOM_NOT_FOUND));
-        log.info("[SUBSCRIBE]: 유효한 채팅방 엔드포인트: {}", chatroomId);
 
-        String username = sessionManager.getUsername(accessor);
-        User user = userRepository.findByUsername(username)
-                .orElseThrow(() -> new NotFoundException(UserResponse.USER_NOT_FOUND));
-        log.info("[SUBSCRIBE]: 유저 조회 성공: {}", username);
+        String destination = accessor.getDestination();
+        if (Objects.isNull(destination)) {
+            throw new NotFoundException(StompResponse.DESTINATION_NOT_FOUND);
+        }
 
-        chatroomValidator.validateSubscribe(user, chatroom);
-        log.info("[SUBSCRIBE]: 유저 `{}`가 채팅방[{}] 구독 완료", username, chatroom.getId());
-
-        subscribeService.subscribe(chatroomId, username, accessor.getSessionId(), accessor.getSubscriptionId());
+        if (destination.startsWith(SubscribeEndpoint.CHATROOM_SUBSCRIBE_PREFIX)) {
+            subscribeValidator.validateChatroomSubscribe(accessor);
+        } else if (destination.startsWith(SubscribeEndpoint.JOINED_CHATROOM_LIST_PREFIX)) {
+            subscribeValidator.validateJoinedChatroomSubscribe(accessor);
+        } else {
+            throw new BadRequestException(StompResponse.DESTINATION_INVALID);
+        }
+        log.info("[SUBSCRIBE]: 구독 종료---------------------------");
     }
 }
