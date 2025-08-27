1309a8955b4a42470332afbcbf470260640f8306
belight2 <chlwnsgur090@gmail.com>
2025-08-27 18:56:03 +0900

    feat: 하트비트 설정 추가

--- src/main/java/com/poortorich/websocket/config/WebSocketConfig.java
+++ src/main/java/com/poortorich/websocket/config/WebSocketConfig.java
@@ -1,7 +1,10 @@
 package com.poortorich.websocket.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -13,14 +16,30 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/chat-websocket")
-                .setAllowedOrigins("*");
-        //        .withSockJS();
+                .setAllowedOrigins("*")
+                .withSockJS()
+                .setHeartbeatTime(25000)
+                .setDisconnectDelay(30000);
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/sub");
+        registry.enableSimpleBroker("/sub")
+                .setHeartbeatValue(new long[]{20_000, 20_000})
+                .setTaskScheduler(heartBeatScheduler());
+
         registry.setApplicationDestinationPrefixes("/pub");
         registry.setUserDestinationPrefix("/chat/user");
+    }
+
+    @Bean
+    public TaskScheduler heartBeatScheduler() {
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setPoolSize(1);
+        scheduler.setThreadNamePrefix("wss-heartbeat-");
+        scheduler.setWaitForTasksToCompleteOnShutdown(true);
+        scheduler.setAwaitTerminationSeconds(30);
+        scheduler.initialize();
+        return scheduler;
     }
 }
