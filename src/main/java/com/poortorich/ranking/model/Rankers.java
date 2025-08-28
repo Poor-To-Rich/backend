7eab027e7df683e2486e2c7cb8f5481c4d93c137
belight2 <chlwnsgur090@gmail.com>
2025-08-28 20:03:39 +0900

    feat: 랭킹 관련 객체 추가

--- src/main/java/com/poortorich/ranking/model/Rankers.java
+++ src/main/java/com/poortorich/ranking/model/Rankers.java
@@ -1,4 +1,65 @@
 package com.poortorich.ranking.model;
 
+import com.poortorich.chat.entity.ChatParticipant;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@Builder
 public class Rankers {
+
+    private static int FIRST = 0;
+    private static int SECOND = 1;
+    private static int THIRD = 2;
+
+    List<ChatParticipant> savers;
+    List<ChatParticipant> flexers;
+
+    private boolean isInvalidAccess(List<ChatParticipant> participants, int index) {
+        return participants == null || participants.size() <= index;
+    }
+
+    public Long firstSaver() {
+        if (isInvalidAccess(savers, FIRST)) {
+            return null;
+        }
+        return savers.get(FIRST).getUser().getId();
+    }
+
+    public Long secondSaver() {
+        if (isInvalidAccess(savers, SECOND)) {
+            return null;
+        }
+        return savers.get(SECOND).getUser().getId();
+    }
+
+    public Long thirdSaver() {
+        if (isInvalidAccess(savers, THIRD)) {
+            return null;
+        }
+        return savers.get(THIRD).getUser().getId();
+    }
+
+    public Long firstFlexer() {
+        if (isInvalidAccess(flexers, FIRST)) {
+            return null;
+        }
+        return flexers.get(FIRST).getUser().getId();
+    }
+
+    public Long secondFlexer() {
+        if (isInvalidAccess(flexers, SECOND)) {
+            return null;
+        }
+        return flexers.get(SECOND).getUser().getId();
+    }
+
+    public Long thirdFlexer() {
+        if (isInvalidAccess(flexers, THIRD)) {
+            return null;
+        }
+        return flexers.get(THIRD).getUser().getId();
+    }
 }
