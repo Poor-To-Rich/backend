0472720e951da3c38823eb3b1d3b356a7ab051db
belight2 <chlwnsgur090@gmail.com>
2025-08-28 20:09:24 +0900

    feat: 랭킹 집계 로직 추가

--- src/main/java/com/poortorich/ranking/facade/RankingFacade.java
+++ src/main/java/com/poortorich/ranking/facade/RankingFacade.java
@@ -2,42 +2,74 @@ package com.poortorich.ranking.facade;
 
 import com.poortorich.chat.entity.ChatParticipant;
 import com.poortorich.chat.entity.Chatroom;
+import com.poortorich.chat.entity.enums.RankingStatus;
+import com.poortorich.chat.realtime.event.user.UserProfileUpdateEvent;
+import com.poortorich.chat.service.ChatMessageService;
 import com.poortorich.chat.service.ChatParticipantService;
 import com.poortorich.chat.service.ChatroomService;
 import com.poortorich.chat.validator.ChatParticipantValidator;
 import com.poortorich.ranking.entity.Ranking;
+import com.poortorich.ranking.model.Rankers;
+import com.poortorich.ranking.payload.response.RankingResponsePayload;
 import com.poortorich.ranking.response.LatestRankingResponse;
 import com.poortorich.ranking.service.RankingService;
 import com.poortorich.ranking.util.RankingBuilder;
+import com.poortorich.ranking.util.calculator.RankingCalculator;
 import com.poortorich.user.entity.User;
 import com.poortorich.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.DayOfWeek;
 import java.time.LocalDateTime;
 import java.time.temporal.TemporalAdjusters;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class RankingFacade {
-
-    public record LatestRankingResult(boolean found, LatestRankingResponse response) {
-        public static LatestRankingResult found(LatestRankingResponse response) {
-            return new LatestRankingResult(true, response);
-        }
-
-        public static LatestRankingResult notFound(LatestRankingResponse response) {
-            return new LatestRankingResult(false, response);
-        }
-    }
 
     private final UserService userService;
     private final ChatroomService chatroomService;
     private final ChatParticipantService chatParticipantService;
     private final ChatParticipantValidator chatParticipantValidator;
     private final RankingService rankingService;
+
+    private final RankingCalculator rankingCalculator;
+    private final ChatMessageService chatMessageService;
+    private final ApplicationEventPublisher eventPublisher;
+
+    @Transactional
+    public RankingResponsePayload calculateRankingTest(Long chatroomId) {
+        Chatroom chatroom = chatroomService.findById(chatroomId);
+        return calculateRanking(chatroom);
+    }
+    
+    @Transactional
+    public RankingResponsePayload calculateRanking(Chatroom chatroom) {
+        Rankers rankers = rankingCalculator.calculate(chatroom);
+        Ranking ranking = rankingService.create(chatroom, rankers);
+
+        List<ChatParticipant> participants = chatParticipantService.findAllByChatroom(chatroom);
+
+        ChatParticipant prevSaver = chatParticipantService.findByChatroomAndRankingStatus(
+                chatroom,
+                RankingStatus.SAVER);
+
+        ChatParticipant prevFLexer = chatParticipantService.findByChatroomAndRankingStatus(
+                chatroom,
+                RankingStatus.FLEXER);
+
+        chatParticipantService.updateAllRankingStatus(participants, RankingStatus.NONE);
+        rankingService.updateRankingStatus(rankers);
+
+        eventPublisher.publishEvent(new UserProfileUpdateEvent(prevSaver.getUser().getUsername()));
+        eventPublisher.publishEvent(new UserProfileUpdateEvent(prevFLexer.getUser().getUsername()));
+
+        return chatMessageService.saveRankingMessage(chatroom, ranking);
+    }
 
     @Transactional(readOnly = true)
     public LatestRankingResult getLatestRanking(String username, Long chatroomId) {
@@ -76,5 +108,15 @@ public class RankingFacade {
 
         LatestRankingResponse response = RankingBuilder.buildLatestRankingResponse(latestRanking, saver, flexer);
         return LatestRankingResult.found(response);
+    }
+
+    public record LatestRankingResult(boolean found, LatestRankingResponse response) {
+        public static LatestRankingResult found(LatestRankingResponse response) {
+            return new LatestRankingResult(true, response);
+        }
+
+        public static LatestRankingResult notFound(LatestRankingResponse response) {
+            return new LatestRankingResult(false, response);
+        }
     }
 }
--- src/main/java/com/poortorich/ranking/service/RankingService.java
+++ src/main/java/com/poortorich/ranking/service/RankingService.java
@@ -1,12 +1,18 @@
 package com.poortorich.ranking.service;
 
+import com.poortorich.chat.entity.ChatParticipant;
 import com.poortorich.chat.entity.Chatroom;
+import com.poortorich.chat.entity.enums.RankingStatus;
 import com.poortorich.ranking.entity.Ranking;
+import com.poortorich.ranking.model.Rankers;
 import com.poortorich.ranking.repository.RankingRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
@@ -17,5 +23,43 @@ public class RankingService {
     public Ranking findLatestRanking(Chatroom chatroom, LocalDateTime start, LocalDateTime end) {
         return rankingRepository.findFirstByChatroomAndCreatedDateBetweenOrderByCreatedDateDesc(chatroom, start, end)
                 .orElse(null);
+    }
+
+    public Ranking create(Chatroom chatroom, Rankers rankers) {
+        if (rankers == null) {
+            return null;
+        }
+
+        Ranking ranking = Ranking.builder()
+                .chatroom(chatroom)
+                .saverFirst(rankers.firstSaver())
+                .saverSecond(rankers.secondSaver())
+                .saverThird(rankers.thirdSaver())
+                .flexerFirst(rankers.firstFlexer())
+                .flexerSecond(rankers.secondFlexer())
+                .flexerThird(rankers.thirdFlexer())
+                .build();
+
+        return rankingRepository.save(ranking);
+    }
+
+    @Transactional
+    public void updateAll(List<ChatParticipant> participants, RankingStatus rankingStatus) {
+        participants.forEach(participant -> participant.updateRankingStatus(rankingStatus));
+    }
+
+    @Transactional
+    public void updateRankingStatus(Rankers rankers) {
+        if (Objects.isNull(rankers)) {
+            return;
+        }
+
+        if (!Objects.isNull(rankers.firstSaver())) {
+            rankers.getSavers().getFirst().updateRankingStatus(RankingStatus.SAVER);
+        }
+
+        if (!Objects.isNull(rankers.secondFlexer())) {
+            rankers.getFlexers().getFirst().updateRankingStatus(RankingStatus.FLEXER);
+        }
     }
 }
