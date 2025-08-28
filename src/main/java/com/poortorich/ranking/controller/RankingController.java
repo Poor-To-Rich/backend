650a2dc4498c9ad18148f8e782c24f2fd7b48fdd
belight2 <chlwnsgur090@gmail.com>
2025-08-28 20:10:34 +0900

    feat: 랭킹 기능 테스트용 REST API 추가

--- src/main/java/com/poortorich/ranking/controller/RankingController.java
+++ src/main/java/com/poortorich/ranking/controller/RankingController.java
@@ -5,11 +5,13 @@ import com.poortorich.global.response.DataResponse;
 import com.poortorich.ranking.facade.RankingFacade;
 import com.poortorich.ranking.response.enums.RankingResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,5 +32,16 @@ public class RankingController {
                 ? RankingResponse.GET_LATEST_RANKING_SUCCESS
                 : RankingResponse.GET_LATEST_RANKING_NOT_FOUND;
         return DataResponse.toResponseEntity(code, result.response());
+    }
+
+    @PostMapping("/test/calculate")
+    public ResponseEntity<BaseResponse> calculateRankingTest(
+            @AuthenticationPrincipal UserDetails userDetails,
+            @PathVariable Long chatroomId
+    ) {
+        return DataResponse.toResponseEntity(
+                HttpStatus.OK,
+                "랭킹 집계 테스트 성공",
+                rankingFacade.calculateRankingTest(chatroomId));
     }
 }
