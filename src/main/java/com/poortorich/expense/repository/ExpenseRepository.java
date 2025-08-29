065e54847dd02df9eacba7488b69c66ccbf6574f
belight2 <chlwnsgur090@gmail.com>
2025-08-29 12:26:23 +0900

    style: 패키지 임포트 경로 변경

--- src/main/java/com/poortorich/expense/repository/ExpenseRepository.java
+++ src/main/java/com/poortorich/expense/repository/ExpenseRepository.java
@@ -4,11 +4,11 @@ import com.poortorich.category.entity.Category;
 import com.poortorich.expense.entity.Expense;
 import com.poortorich.ranking.model.UserExpenseAggregate;
 import com.poortorich.user.entity.User;
-import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
