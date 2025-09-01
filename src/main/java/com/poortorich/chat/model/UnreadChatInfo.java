83bd9cdb66f7acfa3e071a9831d78a6d7473bc2e
belight2 <chlwnsgur090@gmail.com>
2025-09-02 01:09:52 +0900

    feat: 애너테이션 추가

--- src/main/java/com/poortorich/chat/model/UnreadChatInfo.java
+++ src/main/java/com/poortorich/chat/model/UnreadChatInfo.java
@@ -1,10 +1,12 @@
 package com.poortorich.chat.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
+@AllArgsConstructor
 public class UnreadChatInfo {
 
     private final Long chatroomId;
