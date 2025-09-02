9bfe077233c1f910a37718939da250c282c11e84
belight2 <chlwnsgur090@gmail.com>
2025-09-02 23:37:17 +0900

    style: 오탈자 수정

--- src/main/java/com/poortorich/s3/response/enums/S3Response.java
+++ src/main/java/com/poortorich/s3/response/enums/S3Response.java
@@ -11,7 +11,7 @@ import org.springframework.http.HttpStatus;
 public enum S3Response implements Response {
 
     INVALID_FILE_TYPES(HttpStatus.BAD_REQUEST, S3ResponseMessages.INVALID_FILE_TYPE, "profileImage"),
-    FILE_SIZE_TO_LARGE(HttpStatus.PAYLOAD_TOO_LARGE, S3ResponseMessages.FILE_SIZE_TO_LARGE, "profileImage"),
+    FILE_SIZE_TOO_LARGE(HttpStatus.PAYLOAD_TOO_LARGE, S3ResponseMessages.FILE_SIZE_TOO_LARGE, "profileImage"),
     FILE_UPLOAD_FAILURE(HttpStatus.INTERNAL_SERVER_ERROR, S3ResponseMessages.FILE_UPLOAD_FAILURE, "profileImage");
 
     private final HttpStatus httpStatus;
