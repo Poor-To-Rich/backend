a1e3daa8da4ab3964906f604b0a8e6b75695c95f
belight2 <chlwnsgur090@gmail.com>
2025-09-17 15:13:57 +0900

    feat: webp 이미지 변환 우회 로직 추가

--- src/main/java/com/poortorich/s3/util/WebpConverter.java
+++ src/main/java/com/poortorich/s3/util/WebpConverter.java
@@ -4,10 +4,12 @@ import com.poortorich.global.exceptions.InternalServerErrorException;
 import com.poortorich.s3.response.enums.S3Response;
 import com.sksamuel.scrimage.ImmutableImage;
 import com.sksamuel.scrimage.webp.WebpWriter;
-import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Objects;
 
 @Component
 @RequiredArgsConstructor
@@ -15,6 +17,11 @@ public class WebpConverter {
 
     public byte[] convertToWebp(MultipartFile file) {
         try {
+            String contentType = file.getContentType();
+            if (Objects.nonNull(contentType) && contentType.equals("image/webp")) {
+                return file.getBytes();
+            }
+
             return ImmutableImage.loader().fromBytes(file.getBytes())
                     .bytes(WebpWriter.DEFAULT.withQ(80));
         } catch (IOException exception) {
