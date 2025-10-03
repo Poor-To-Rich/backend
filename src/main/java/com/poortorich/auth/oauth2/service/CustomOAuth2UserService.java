00edaeda967f71de37e7cd47478ab0e514868ec5
belight2 <chlwnsgur090@gmail.com>
2025-10-03 20:48:06 +0900

    feat: 카카오 기존 회원 존재시 처리 로직 추가

--- src/main/java/com/poortorich/auth/oauth2/service/CustomOAuth2UserService.java
+++ src/main/java/com/poortorich/auth/oauth2/service/CustomOAuth2UserService.java
@@ -2,13 +2,12 @@ package com.poortorich.auth.oauth2.service;
 
 import com.poortorich.auth.oauth2.domain.model.CustomOAuth2UserDetails;
 import com.poortorich.auth.oauth2.domain.model.KakaoResponse;
+import com.poortorich.auth.oauth2.redis.KakaoRedisService;
 import com.poortorich.category.service.CategoryService;
 import com.poortorich.user.entity.User;
 import com.poortorich.user.entity.enums.Gender;
 import com.poortorich.user.entity.enums.Role;
 import com.poortorich.user.repository.UserRepository;
-import java.time.LocalDate;
-import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
@@ -16,12 +15,17 @@ import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.util.Optional;
+import java.util.UUID;
+
 @Service
 @RequiredArgsConstructor
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
 
     private final UserRepository userRepository;
     private final CategoryService categoryService;
+    private final KakaoRedisService kakaoRedisService;
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest request) throws OAuth2AuthenticationException {
@@ -34,8 +38,21 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
 
     private User saveUser(KakaoResponse response) {
         String username = response.getProvider() + "_" + response.getProviderId();
-        return userRepository.findByEmail(response.getEmail())
-                .orElseGet(() -> toUserEntity(response, username));
+        Optional<User> userOptional = userRepository.findByEmail(response.getEmail());
+
+        if (userOptional.isPresent()) {
+            User user = userOptional.get();
+            if (username.equals(user.getIdentify())) {
+                return user;
+            }
+            
+            kakaoRedisService.saveUser(user);
+            user.updateIdentify(username);
+            user.updateRole(Role.KAKAO_EXISTING_USER_PENDING);
+            return userRepository.save(user);
+        }
+
+        return toUserEntity(response, username);
     }
 
     private User toUserEntity(KakaoResponse response, String username) {
@@ -50,6 +67,7 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
                 .birth(LocalDate.now())
                 .profileImage(response.getProfileImage())
                 .role(Role.PENDING)
+                .identify(username)
                 .job("선택안함")
                 .build();
 
