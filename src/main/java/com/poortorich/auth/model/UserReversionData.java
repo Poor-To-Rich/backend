4d6dbea436ed1f25320d22ad7db7a176abe1588f
belight2 <chlwnsgur090@gmail.com>
2025-10-03 20:46:29 +0900

    feat: 카카오 레디스 서비스 추가

--- src/main/java/com/poortorich/auth/model/UserReversionData.java
+++ src/main/java/com/poortorich/auth/model/UserReversionData.java
@@ -1,4 +1,25 @@
 package com.poortorich.auth.model;
 
+import com.poortorich.user.entity.enums.Gender;
+import com.poortorich.user.entity.enums.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserReversionData {
+
+    private String name = null;
+    private String nickname = null;
+    private String email = null;
+    private Gender gender = null;
+    private Role role = null;
+    private String profileImage = null;
+    private String identify = null;
+    private String username = null;
+    private String password = null;
 }
--- src/main/java/com/poortorich/auth/oauth2/redis/KakaoRedisService.java
+++ src/main/java/com/poortorich/auth/oauth2/redis/KakaoRedisService.java
@@ -1,4 +1,76 @@
 package com.poortorich.auth.oauth2.redis;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.poortorich.auth.model.UserReversionData;
+import com.poortorich.global.exceptions.InternalServerErrorException;
+import com.poortorich.global.response.enums.GlobalResponse;
+import com.poortorich.user.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class KakaoRedisService {
+
+    private static final String KAKAO_USER_KEY_SUFFIX = "kakao:user:";
+
+    private final StringRedisTemplate redisTemplate;
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    public void saveUser(User user) {
+        UserReversionData data = UserReversionData.builder()
+                .name(user.getName())
+                .username(user.getUsername())
+                .password(user.getPassword())
+                .nickname(user.getNickname())
+                .email(user.getEmail())
+                .gender(user.getGender())
+                .profileImage(user.getProfileImage())
+                .role(user.getRole())
+                .identify(user.getIdentify())
+                .build();
+        try {
+            String key = getKakaoUserKey(user.getId());
+            String value = objectMapper.writeValueAsString(data);
+            redisTemplate.opsForValue().set(key, value);
+        } catch (JsonProcessingException exception) {
+            throw new InternalServerErrorException(GlobalResponse.INTERNAL_SERVER_EXCEPTION);
+        }
+    }
+
+    public UserReversionData getData(User user) {
+        try {
+            String key = getKakaoUserKey(user.getId());
+            String value = redisTemplate.opsForValue().get(key);
+            if (value == null) {
+                System.out.println("데이터를 찾을 수 없다.");
+                return UserReversionData.builder()
+                        .name(user.getName())
+                        .username(user.getUsername())
+                        .password(user.getPassword())
+                        .nickname(user.getNickname())
+                        .email(user.getEmail())
+                        .gender(user.getGender())
+                        .profileImage(user.getProfileImage())
+                        .role(user.getRole())
+                        .identify(user.getIdentify())
+                        .build();
+            }
+            return objectMapper.readValue(value, UserReversionData.class);
+        } catch (JsonProcessingException exception) {
+            System.out.println(exception.getMessage());
+            throw new InternalServerErrorException(GlobalResponse.INTERNAL_SERVER_EXCEPTION);
+        }
+    }
+
+    public void deleteUser(User user) {
+        String key = getKakaoUserKey(user.getId());
+        redisTemplate.delete(key);
+    }
+
+    private String getKakaoUserKey(Long userId) {
+        return KAKAO_USER_KEY_SUFFIX + userId;
+    }
 }
